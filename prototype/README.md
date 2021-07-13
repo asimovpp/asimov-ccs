aea82fbd99a9138840af08f18c804b99dcf0529b
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-07-13 09:07:13 +0100

    Add configurable linker

---
+++ prototype/README.md
@@ -0,0 +1,23 @@
+# How to run asimov-ccs
+  export FC=${my_fortran_compiler}
+  make all
+  python3 link_executable.py config.json
+  ./ccs_main
+
+# Customising asimov-ccs
+  Edit config.json
+  
+  Valid options:
+  - turbulance
+    - ke
+    - kw 
+  - particles
+    - 1
+    - 2
+  - flux
+    - 1st_order
+    - 2nd_order
+  - solver
+    - amg
+    - cgstab
+
---
+++ prototype/link_executable.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import json
+
+method_file_mapping = \
+{
+  "turbulance": {"ke": "turbulence_ke",
+                 "kw": "turbulence_kw"},
+  "particles" : {"1": "particles_1",
+                 "2": "particles_2"},
+  "flux"      : {"1st_order": "flux_1order",
+                 "2nd_order": "flux_2order"},
+  "solver"    : {"amg": "solve_amg",
+                 "cgstab": "solve_cgstab"}
+}
+
+if __name__ == "__main__":
+  with open(sys.argv[1]) as f:
+    config = json.load(f)
+  print("config read: ", config)
+
+  link_command = os.environ["FC"] + " -o ccs_main main.o presolve.o postsolve.o flowsolve.o presolve_basic.o postsolve_basic.o" 
+  for k,v in config.items():
+    print("parsing config item:", method_file_mapping[k][v])
+    link_command = link_command + " " + method_file_mapping[k][v] + ".o"
+  print("link command: ", link_command)
+
+  os.system(link_command) 
+
+  
