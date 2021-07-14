bc21df6f7141afe7455d12107b50dc83479ddd5e
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-07-14 14:53:44 +0100

    Factor out top level stepper and add 2 options for it

--- prototype/main.f90
+++ prototype/main.f90
@@ -3,20 +3,11 @@ use presolve
 use postsolve
 use flowsolve
 implicit none
-integer :: timestep, solvestep
 
 call read_input()
 call do_initialisation()
 
-do timestep = 1, 3 
-  call do_time_step_stuff()
-  do solvestep = 1, 2
-    call turbulence()
-    call particles()
-    call flux()
-    call solve()
-  end do
-end do
+call stepper()
 
 call write_output()
 call do_finalisation()
