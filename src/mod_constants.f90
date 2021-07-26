c3b57bbb64c1bd8ef0710982b97d50cd9e1f37d7
Paul <p.bartholomew@epcc.ed.ac.uk>
2021-07-26 22:22:15 +0000

    Start of develop branch
    Adding a minimal src/ directory with an example module to defined
    some constants

---
+++ src/mod_constants.f90
@@ -0,0 +1,13 @@
+!!!        FILE: mod_constants.f90
+!!! DESCRIPTION: Module defining constants used by ASiMoV-CCS
+
+module constants
+
+  use iso_fortran_env
+  
+  implicit none
+
+  !! Basic types
+  integer, parameter :: accs_real = real64
+  
+end module constants
