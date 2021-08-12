af488bd72d35b7600ef10731f57bfe07bc278ff3
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-08-12 09:45:11 +0100

    Initial commit prototype ex3
    Currently just the main program file with high-level comments
    outlining the steps of the program and a Makefile to build it.

---
+++ prototypes/prototype_petsc-ex3/src/ex3.f90
@@ -0,0 +1,23 @@
+!!! -*- mode: F90 -*-
+!!! vim: set syntax=fortran:
+!!!
+!!!        FILE: ex3.f90
+!!! DESCRIPTION: Port of PETSc ksp/tutorial/ex3.c to ASiMoV-CCS style code - this is to help
+!!!              determine how to interface our design with PETSc.
+!!!
+
+program ex3
+
+  implicit none
+
+  !! Create stiffness matrix
+  !! Assemble matrix
+  !! Create right-hand-side and solution vectors
+  !! Assemble right-hand-side vector
+  !! Modify matrix and right-hand-side vector to apply Dirichlet boundary conditions
+  !! Create linear solver & set options
+  !! Solve linear system
+  !! Check solution
+  !! Clean up
+  
+end program ex3
