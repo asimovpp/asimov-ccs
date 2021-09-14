e4f9c1cd17191f359ba065f202bf16cb2cac1e48
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-09-14 17:43:48 +0100

    Fix setting matrix coefficients for FVM
    Was deallocating in the wrong place, and forgot that I need to
    convert Fortran indices to C indices (for now - this should be fixed
    eventually).

--- prototypes/prototype_petsc-ex3/src/ex3.f90
+++ prototypes/prototype_petsc-ex3/src/ex3.f90
@@ -1,7 +1,15 @@
 !> @brief Program file PETSc ex3
-!>
+!
 !> @details Port of PETSc ksp/tutorial/ex3.c to ASiMoV-CCS style code - this is to help
-!>          determine how to interface our design with PETSc.
+!!          determine how to interface our design with PETSc.
+!!          Solves the equation
+!!          \f[
+!!            {\nabla^2} p = f
+!!          \f]
+!!          in the unit square with Dirichlet boundary conditions
+!!          \f[
+!!            p\left(\boldsymbol{x}\right) = y,\ \boldsymbol{x}\in\partial\Omega
+!!          \f]
 
 program ex3
 
@@ -38,7 +46,7 @@ program ex3
   !! The innards of a rudimentary mesh structure
   integer(accs_int) :: nc
   integer(accs_int) :: istart, iend
-  real(accs_real) :: h, Af
+  real(accs_real) :: h, Af, vol
   integer(accs_int), dimension(:), allocatable :: nnb
   integer(accs_int), dimension(:, :), allocatable :: nbidx
   !!=========================================================
@@ -51,17 +59,21 @@ program ex3
   !! Initialise program
   call accs_init()
   call init_ex3()
-  
+
+  print *, "Create stiffness matrix"
   !! Create stiffness matrix
   mat_sizes%rglob = nc
   mat_sizes%cglob = mat_sizes%rglob
   mat_sizes%comm = PETSC_COMM_WORLD
-  mat_sizes%nnz = 9
+  mat_sizes%nnz = 5
   call create_matrix(mat_sizes, M)
 
+  print *, "Disc Poisson"
   call discretise_poisson(M)
+  print *, "Done"
   call update(M) ! Performs the parallel assembly
 
+  print *, "Create vectors"
   !! Create right-hand-side and solution vectors
   vec_sizes%nloc = -1
   vec_sizes%nglob = nc
@@ -69,14 +81,17 @@ program ex3
   call create_vector(vec_sizes, u)
   call create_vector(vec_sizes, b)
   call update(u) ! Performs the parallel assembly
-  
+
+  print *, "Eval RHS"
   !! Evaluate right-hand-side vector
   call eval_rhs(b)
   call update(b) ! Performs the parallel assembly
-  
+
+  print *, "Apply Dirichlet BCs"
   !! Modify matrix and right-hand-side vector to apply Dirichlet boundary conditions
   call apply_dirichlet_bcs(M, b, u)
-  
+
+  print *, "Setup solver"
   !! Create linear solver & set options
   poisson_eq%rhs => b
   poisson_eq%sol => u
@@ -84,7 +99,8 @@ program ex3
   poisson_eq%comm = PETSC_COMM_WORLD
   call create_solver(poisson_eq, poisson_solver)
   call solve(poisson_solver)
-  
+
+  print *, "Solve and check"
   !! Check solution
   call create_vector(vec_sizes, ustar)
   call set_exact_sol(ustar)
@@ -117,17 +133,15 @@ contains
     real(accs_real) :: x, y
 
     type(vector_values) :: val_dat
-
-    stop "eval_rhs not converted to FVM"
     
     val_dat%mode = add_mode
     allocate(val_dat%idx(1))
     allocate(val_dat%val(1))
     
     do i = istart, iend
-       ii = i - 1
-       x = h * modulo(ii, cps)
-       y = h * (ii / cps)
+       ii = i - 1              ! This code is translated from C - shift back by 1
+       x = h * (modulo(ii, cps) + 0.5)
+       y = h * ((ii / cps) + 0.5)
 
        call eval_cell_rhs(x, y, h**2, val_dat%val(1))
        call set_values(val_dat, b)
@@ -146,48 +160,59 @@ contains
     
     r = 0.0 &
          + 0 * (x + y + H) ! Silence unused dummy argument error
+    r = vol * r
     
   end subroutine eval_cell_rhs
 
   subroutine discretise_poisson(M)
 
-    use accs_constants, only : add_mode
+    use accs_constants, only : insert_mode
     use accs_types, only : matrix_values
     use accs_utils, only : set_values
     
     class(matrix), intent(inout) :: M
 
     type(matrix_values) :: mat_coeffs
-    integer(accs_int) :: i, j
+    integer(accs_int) :: i, ii, j
 
     real(accs_real) :: coeff_f
     
-    mat_coeffs%mode = add_mode
+    mat_coeffs%mode = insert_mode
 
     !! Loop over cells
+    ii = 1
     do i = istart, iend
-       allocate(mat_coeffs%rglob(1 + nnb(i)), &
-            mat_coeffs%cglob(1 + nnb(i)), &
-            mat_coeffs%val(1 + nnb(i)))
-       mat_coeffs%rglob(:) = i
+       !> @todo: Doing this in a loop is awful code - malloc maximum coefficients per row once,
+       !!        filling from front, and pass the number of coefficients to be set, requires
+       !!        modifying the matrix_values type and the implementation of set_values applied to
+       !!        matrices.
+       allocate(mat_coeffs%rglob(1), &
+            mat_coeffs%cglob(1 + nnb(ii)))
+       allocate(mat_coeffs%val(1 + nnb(ii)))
+       
+       mat_coeffs%rglob(1) = i
+       mat_coeffs%cglob(:) = -1 ! -ve indices are ignored
+       mat_coeffs%cglob(1) = i
+       mat_coeffs%val(:) = 0.0_accs_real
        !! Loop over faces
-       do j = 1, nnb(i)
-          if (nbidx(i, j) >= 0) then
+       do j = 1, nnb(ii)
+          if (nbidx(j, ii) >= 0) then
              !! Interior face
              coeff_f = (1.0 / h) * Af
              mat_coeffs%val(1) = mat_coeffs%val(1) - coeff_f
              mat_coeffs%val(j + 1) = coeff_f
-             mat_coeffs%cglob(j + 1) = nbidx(i, j)
+             mat_coeffs%cglob(j + 1) = nbidx(j, ii)
           end if
        end do
-       deallocate(mat_coeffs%rglob, mat_coeffs%cglob, mat_coeffs%val)
-       mat_coeffs%cglob = mat_coeffs%rglob
+       mat_coeffs%rglob(:) = mat_coeffs%rglob(:) - 1
+       mat_coeffs%cglob(:) = mat_coeffs%cglob(:) - 1
        call set_values(mat_coeffs, M)
-    end do
 
-    deallocate(mat_coeffs%rglob)
-    deallocate(mat_coeffs%cglob)
-    deallocate(mat_coeffs%val)
+       deallocate(mat_coeffs%rglob, mat_coeffs%cglob, mat_coeffs%val)
+
+       ii = ii + 1
+    end do
+    print *, "Finished disc Poiss"
     
   end subroutine discretise_poisson
 
@@ -222,14 +247,14 @@ contains
                !! Boundary face
 
                ! Coefficient
-               row(1) = i
-               col(1) = i
+               row(1) = i - 1
+               col(1) = i - 1
                coeff(1) = -(3 / h) * Af
                call set_values(mat_coeffs, M)
                
                ! RHS vector
-               idx(1) = i
-               val(1) = h * (i / (cps + 1))
+               idx(1) = i - 1
+               val(1) = h * (i / (cps + 1) + 0.5) ! XXX: This is sort of y (y=i/cps+0.5), but matches ex3.c...
                val(1) = -(val(1) / h) * Af
                call set_values(vec_values, b)
             end if
@@ -261,10 +286,8 @@ contains
     type(vector_values) :: vec_values
     integer(accs_int) :: i, istart, iend
     integer(accs_err) :: ierr
-
-    stop "set_exact_sol not converted to FVM"
     
-    ! TODO: abstract this!
+    !> @todo: abstract this!
     select type(ustar)
     type is (vector_petsc)
        call VecGetOwnershipRange(ustar%v, istart, iend, ierr)
@@ -279,7 +302,7 @@ contains
     vec_values%mode = insert_mode
     do i = istart, iend
        vec_values%idx(1) = i - 1
-       vec_values%val(1) = h * (vec_values%idx(1) / (cps + 1))
+       vec_values%val(1) = h * (vec_values%idx(1) / (cps + 1) + 0.5)
        call set_values(vec_values, ustar)
     end do
     deallocate(vec_values%idx)
@@ -290,10 +313,15 @@ contains
 
   subroutine init_ex3()
 
-    stop "init_ex3 not converted to FVM"
+    integer(accs_int) :: i, ii
+    integer(accs_int) :: nowned
     
     h = 1.0_accs_real / cps
+    Af = h            ! Face area
+    vol = h**2        ! Cell volume
     nc = cps**2       ! Number of cells
+
+    !! Setup ownership range
     call MPI_Comm_size(PETSC_COMM_WORLD, comm_size, ierr)
     call MPI_Comm_rank(PETSC_COMM_WORLD, comm_rank, ierr)
     istart = comm_rank * (nc / comm_size)
@@ -309,6 +337,44 @@ contains
        iend = iend + 1
     end if
     iend = iend - 1
+
+    nowned = (iend - istart) + 1
+    allocate(nnb(nowned))
+    nnb(:) = 4                    ! All cells have 4 neighbours (possibly ghost/boundary cells)
+    allocate(nbidx(4, nowned))
+
+    !! Get neighbour indices
+    do i = istart, iend
+       ii = i - 1
+       
+       !! Left neighbour
+       if (modulo(ii, cps) == 0) then
+          nbidx(1, i) = -1
+       else
+          nbidx(1, i) = i - 1
+       end if
+
+       !! Right neighbour
+       if (modulo(ii, cps) == (cps - 1)) then
+          nbidx(2, i) = -2
+       else
+          nbidx(2, i) = i + 1
+       end if
+
+       !! Down neighbour
+       if ((ii / cps) == 0) then
+          nbidx(3, i) = -3
+       else
+          nbidx(3, i) = i - cps
+       end if
+
+       !! Up neighbour
+       if ((ii / cps) == (cps - 1)) then
+          nbidx(4, i) = -4
+       else
+          nbidx(4, i) = i + cps
+       end if
+    end do
     
   end subroutine init_ex3
 end program ex3
