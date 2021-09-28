f40c61433da525808bcbd5eb24446de0c820d656
Michele Weiland <m.weiland@epcc.ed.ac.uk>
2021-09-28 11:52:39 +0100

    Move boundary coniditions code by into poisson.f90
    As it stands, `create_boundary_conditions_poisson.f90` does not compile - it requires access to the `square_mesh` object and the `rhs_val` function. This will be easy to deal with once abstraction is complete. For now, the implementation can live inside `poisson.f90`.

--- src/case_setup/Poisson/poisson.f90
+++ src/case_setup/Poisson/poisson.f90
@@ -28,7 +28,7 @@ program poisson
   use solver, only : create_solver, solve
   use utils, only : accs_init, accs_finalise, update, finalise
 
-  use mesh, only : build_square_mesh
+  use mesh_utils, only : build_square_mesh
   
   use petsctypes, only : matrix_petsc
   
@@ -78,7 +78,7 @@ program poisson
   call update(b) ! Performs the parallel assembly
 
   !! Modify matrix and right-hand-side vector to apply Dirichlet boundary conditions
-  call apply_dirichlet_bcs(M, b)
+  call apply_dirichlet_bcs(M, b, square_mesh)
   call update(b)
   call finalise(M)
   
@@ -215,6 +215,92 @@ contains
     end do
     
   end subroutine discretise_poisson
+
+  subroutine apply_dirichlet_bcs(M, b, square_mesh)
+
+    use constants, only : add_mode
+    use mat, only : set_eqn
+    use types, only : vector_values, matrix_values, matrix, vector, mesh
+    use utils, only : set_values
+    use kinds, only: accs_int, accs_real
+  
+    implicit none
+  
+    class(matrix), intent(inout) :: M
+    class(vector), intent(inout) :: b
+    class(mesh), intent(in) :: square_mesh
+  
+    integer(accs_int) :: i, j
+    real(accs_real) :: boundary_coeff, boundary_val
+  
+    type(vector_values) :: vec_values
+    type(matrix_values) :: mat_coeffs
+  
+    allocate(mat_coeffs%rglob(1), mat_coeffs%cglob(1), mat_coeffs%val(1))
+    allocate(vec_values%idx(1), vec_values%val(1))
+    mat_coeffs%mode = add_mode
+    vec_values%mode = add_mode
+  
+    associate(row=>mat_coeffs%rglob, &
+         col=>mat_coeffs%cglob, &
+         coeff=>mat_coeffs%val, &
+         idx=>vec_values%idx, &
+         val=>vec_values%val, &
+         idx_global=>square_mesh%idx_global)
+  
+      do i = 1, square_mesh%nlocal
+         if (minval(square_mesh%nbidx(:, i)) < 0) then
+            coeff(1) = 0.0_accs_real
+            val(1) = 0.0_accs_real
+            do j = 1, square_mesh%nnb(i)
+               associate(nbidx=>square_mesh%nbidx(j, i))
+  
+                 if (nbidx < 0) then
+                    associate(h=>square_mesh%h, &
+                         Af=>square_mesh%Af)
+  
+                      !! Boundary face
+                      boundary_coeff = (2.0 / h) * Af
+  
+                      if ((nbidx == -1) .or. (nbidx == -2)) then
+                         !! Left or right boundary
+                         boundary_val = rhs_val(idx_global(i))
+                      else if (nbidx == -3) then
+                         !! Bottom boundary
+                         boundary_val = rhs_val(0, -0.5_accs_real)
+                      else if (nbidx == -4) then
+                         !! Top boundary
+                         boundary_val = rhs_val(idx_global(i), 0.5_accs_real)
+                      else
+                         print *, "ERROR: invalid/unsupported BC ", nbidx
+                         stop
+                      end if
+  
+                      ! Coefficient
+                      row(1) = idx_global(i) - 1
+                      col(1) = idx_global(i) - 1
+                      coeff(1) = coeff(1) - boundary_coeff
+  
+                      ! RHS vector
+                      idx(1) = idx_global(i) - 1
+                      val(1) = val(1) - boundary_val * boundary_coeff
+  
+                    end associate
+                 end if
+  
+               end associate
+            end do
+            call set_values(mat_coeffs, M)
+            call set_values(vec_values, b)
+         end if
+      end do
+  
+    end associate
+  
+    deallocate(vec_values%idx)
+    deallocate(vec_values%val)
+  
+  end subroutine apply_dirichlet_bcs
 
   subroutine set_exact_sol(ustar)
 
