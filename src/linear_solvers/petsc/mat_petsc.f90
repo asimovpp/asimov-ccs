b020ab449c399b0890eaee21cfe6356e6a9cbca8
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-10-15 17:30:30 +0100

    Fix double use in matrix PETSc submodule

--- src/linear_solvers/petsc/mat_petsc.f90
+++ src/linear_solvers/petsc/mat_petsc.f90
@@ -1,6 +1,6 @@
 submodule (mat) mat_petsc
 
-  use kinds, only : accs_real, accs_err
+  use kinds, only : accs_err
   use petsctypes, only : matrix_petsc
   use parallel_types_mpi, only: parallel_environment_mpi
   
