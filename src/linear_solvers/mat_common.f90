17b48e57e81aac84d5de93bc1e5b6f3b3ad11506
Michele Weiland <m.weiland@epcc.ed.ac.uk>
2021-11-03 17:59:50 +0000

    Change mat and vec to matrix_descriptor and vector_descriptor in size setting procedures. Also set local/global to -1, analogous to the vector implementation.

--- src/linear_solvers/mat_common.f90
+++ src/linear_solvers/mat_common.f90
@@ -6,58 +6,65 @@ contains
 
   !> @brief Constructor for default matrix values
   !
-  !> param[in/out] mat     - the initialised matrix values
-  module subroutine initialise_matrix(mat)
-    type(matrix_init_data), intent(inout) :: mat
-    mat%rglob = -1
-    mat%cglob = -1
-    mat%rloc = -1
-    mat%cloc = -1
-    mat%nnz = -1
-    mat%par_env => null()
+  !> param[in/out] matrix_descriptor - the initialised matrix values
+  module subroutine initialise_matrix(matrix_descriptor)
+    type(matrix_init_data), intent(inout) :: matrix_descriptor
+    matrix_descriptor%rglob = -1
+    matrix_descriptor%cglob = -1
+    matrix_descriptor%rloc = -1
+    matrix_descriptor%cloc = -1
+    matrix_descriptor%nnz = -1
+    matrix_descriptor%par_env => null()
   end subroutine initialise_matrix
 
   !> @brief Setter for global matrix size
   !
-  !> param[in/out] mat    - the matrix data object
-  !> param[in] rows       - the global number of rows
-  !> param[in] columns    - the global number of columns
-  !> param[in] par_env    - the parallel environment where the matrix resides
-  module subroutine set_global_matrix_size(mat, rows, columns, par_env)
-    type(matrix_init_data), intent(inout) :: mat
+  !> param[in/out] matrix_descriptor  - the matrix data object
+  !> param[in] rows                   - the global number of rows
+  !> param[in] columns                - the global number of columns
+  !> param[in] par_env                - the parallel environment where 
+  !!                                    the matrix resides
+  module subroutine set_global_matrix_size(matrix_descriptor, rows, columns, par_env)
+    type(matrix_init_data), intent(inout) :: matrix_descriptor
     integer(accs_int), intent(in) :: rows
     integer(accs_int), intent(in) :: columns
     class(parallel_environment), allocatable, target, intent(in) :: par_env
-    mat%rglob = rows
-    mat%cglob = columns
-    mat%par_env => par_env
+    matrix_descriptor%rglob = rows
+    matrix_descriptor%cglob = columns
+    matrix_descriptor%rloc = -1
+    matrix_descriptor%cloc = -1
+    matrix_descriptor%par_env => par_env
   end subroutine
 
   !> @brief Setter for local matrix size
   !
-  !> param[in/out] mat    - the matrix data object
-  !> param[in] rows       - the local number of rows
-  !> param[in] columns    - the local number of columns
-  !> param[in] par_env    - the parallel environment where the matrix resides
-  module subroutine set_local_matrix_size(mat, rows, columns, par_env)
-    type(matrix_init_data), intent(inout) :: mat
+  !> param[in/out] matrix_descriptor  - the matrix data object
+  !> param[in] rows                   - the local number of rows
+  !> param[in] columns                - the local number of columns
+  !> param[in] par_env                - the parallel environment where
+  !!                                    the matrix resides
+  module subroutine set_local_matrix_size(matrix_descriptor, rows, columns, par_env)
+    type(matrix_init_data), intent(inout) :: matrix_descriptor
     integer(accs_int), intent(in) :: rows
     integer(accs_int), intent(in) :: columns
     class(parallel_environment), allocatable, target, intent(in) :: par_env
-    mat%rloc = rows
-    mat%cloc = columns
-    mat%par_env => par_env
+    matrix_descriptor%rloc = rows
+    matrix_descriptor%cloc = columns
+    matrix_descriptor%rglob = -1
+    matrix_descriptor%cglob = -1
+    matrix_descriptor%par_env => par_env
   end subroutine
 
   !> @brief Setter for matrix number of non-zeros
   !
-  !> param[in/out] mat    - the matrix data object
-  !> param[in] nnz        - the number of non-zeros
-  !> param[in] par_env    - the parallel environment where the matrix resides
-  module subroutine set_nnz(mat, nnz)
-    type(matrix_init_data), intent(inout) :: mat
+  !> param[in/out] matrix_descriptor - the matrix data object
+  !> param[in] nnz                   - the number of non-zeros
+  !> param[in] par_env               - the parallel environment where
+  !!                                   the matrix resides
+  module subroutine set_nnz(matrix_descriptor, nnz)
+    type(matrix_init_data), intent(inout) :: matrix_descriptor
     integer(accs_int), intent(in) :: nnz
-    mat%nnz = nnz
+    matrix_descriptor%nnz = nnz
   end subroutine  
 
 end submodule
--- src/linear_solvers/vec_common.f90
+++ src/linear_solvers/vec_common.f90
@@ -6,40 +6,42 @@ contains
 
   !> @brief Constructor for default vector values
   !
-  !> param[in/out] vec     - the initialised vector values
-  module subroutine initialise_vector(vec)
-    type(vector_init_data), intent(inout) :: vec
-    vec%nglob = -1
-    vec%nloc = -1
-    vec%par_env => null()
+  !> param[in/out] vector_descriptor - the initialised vector values
+  module subroutine initialise_vector(vector_descriptor)
+    type(vector_init_data), intent(inout) :: vector_descriptor
+    vector_descriptor%nglob = -1
+    vector_descriptor%nloc = -1
+    vector_descriptor%par_env => null()
   end subroutine initialise_vector
 
   !> @brief Setter for global vector size
   !
-  !> param[in/out] vec    - the vector data object
-  !> param[in] size       - the global vector size
-  !> param[in] par_env    - the parallel environment where the vector resides
-  module subroutine set_global_vector_size(vec, size, par_env)
-    type(vector_init_data), intent(inout) :: vec
+  !> param[in/out] vector_descriptor - the vector data object
+  !> param[in] size                  - the global vector size
+  !> param[in] par_env               - the parallel environment 
+  !!                                   where the vector resides
+  module subroutine set_global_vector_size(vector_descriptor, size, par_env)
+    type(vector_init_data), intent(inout) :: vector_descriptor
     integer(accs_int), intent(in) :: size
     class(parallel_environment), allocatable, target, intent(in) :: par_env
-    vec%nglob = size
-    vec%nloc = -1
-    vec%par_env => par_env
+    vector_descriptor%nglob = size
+    vector_descriptor%nloc = -1
+    vector_descriptor%par_env => par_env
   end subroutine
 
   !> @brief Setter for local vector size
   !
-  !> param[in/out] vec    - the vector data object
-  !> param[in] size       - the local vector size
-  !> param[in] par_env    - the parallel environment where the vector resides
-  module subroutine set_local_vector_size(vec, size, par_env)
-    type(vector_init_data), intent(inout) :: vec
+  !> param[in/out] vvector_descriptor - the vector data object
+  !> param[in] size                   - the local vector size
+  !> param[in] par_env                - the parallel environment 
+  !!                                    where the vector resides
+  module subroutine set_local_vector_size(vector_descriptor, size, par_env)
+    type(vector_init_data), intent(inout) :: vector_descriptor
     integer(accs_int), intent(in) :: size
     class(parallel_environment), allocatable, target, intent(in) :: par_env
-    vec%nloc = size
-    vec%nglob = -1
-    vec%par_env => par_env
+    vector_descriptor%nloc = size
+    vector_descriptor%nglob = -1
+    vector_descriptor%par_env => par_env
   end subroutine
 
 end submodule 
