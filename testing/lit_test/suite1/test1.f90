182e39c50a8de3e2023c89996778c51535c80a49
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-12-03 14:53:11 +0000

    Minimal LIT setup

---
+++ testing/lit_test/suite1/test1.f90
@@ -0,0 +1,6 @@
+! RUN: gfortran -o %t1 %s
+! RUN: %t1
+
+program test1
+   STOP 0
+end program test1
---
+++ testing/lit_test/suite1/test2.f90
@@ -0,0 +1,6 @@
+! RUN: gfortran -o %t1 %s
+! RUN: %t1
+
+program test2
+   STOP 1
+end program test2
