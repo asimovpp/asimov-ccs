4f6c9bf2133a47c61e6f65a16a315c90971b31e0
Michele Weiland <m.weiland@epcc.ed.ac.uk>
2021-12-08 12:49:09 +0000

    Removing build scripts

--- src/case_setup/TaylorGreenVortex/build_gnu.sh
+++
@@ -1,5 +0,0 @@
-rm test_yaml_gnu *.o
-
-gfortran -I/work/Projects/ASiMoV/fortran-yaml-cpp/build -c test_yaml.f90 -o test_yaml.o
-
-gfortran test_yaml.o -o test_yaml -Wl,-rpath,/work/Projects/ASiMoV/fortran-yaml-cpp/build:/work/Projects/ASiMoV/fortran-yaml-cpp/build/yaml-cpp -L/work/Projects/ASiMoV/fortran-yaml-cpp/build -lfortran-yaml-cpp -L/work/Projects/ASiMoV/fortran-yaml-cpp/build/yaml-cpp -lyaml-cpp
