9c3dd4e464c7a7d6f5d01c350a801f3372e355ac
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-12-22 15:19:02 +0000

    Improve test builder script
    - Switch bash command running to "subprocess"
    - Check bash command output code; stop if non-zero
    - Add build status printouts

--- testing/lit_test/test_builder.py
+++ testing/lit_test/test_builder.py
@@ -1,6 +1,7 @@
 import sys
 import yaml
 import os
+import subprocess as sp
 
 config_file = sys.argv[1]
 test_obj = sys.argv[2]
@@ -8,26 +9,32 @@ test_deps = sys.argv[3]
 test_link = sys.argv[4]
 test_exe = sys.argv[5]
 
-# get path to test source
+print("TEST BUILDER: starting")
+
+print("TEST BUILDER: getting path to test source")
 with open(config_file, "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
     main = os.path.dirname(config_file) + "/" + config["main"] + ".f90"
 
-# compile test object
-os.system("${FC} ${FFLAGS} -I${OBJ_DIR} ${INC} -c -o " 
-           + test_obj + " " + main)
+print("TEST BUILDER: compiling test object")
+sp.run("${FC} ${FFLAGS} -I${OBJ_DIR} ${INC} -c -o " + test_obj + " " + main,
+       shell=True, check=True)
 
-# evaluate test dependencies
-os.system("makedepf90 ${SRC} " + main + " > " + test_deps)
+print("TEST BUILDER: evaluating test dependencies")
+sp.run("makedepf90 ${SRC} " + main + " > " + test_deps + " 2> " + test_deps + ".err",
+       shell=True, check=True)
 
-# generate link rule for test
-os.system("python3 ${TOOLS}generate_link_deps.py " + config_file + " " + test_deps + " " + test_link)
+print("TEST BUILDER: generating link rule for test")
+sp.run("python3 ${TOOLS}generate_link_deps.py " + config_file + " " + test_deps + " " + test_link,
+       shell=True, check=True)
 
-# strip off target and test object file from link rule
+print("TEST BUILDER: stripping off target and test object file from link rule")
 with open(test_link, "r") as f:
     link_rule = f.readline().strip()
 link_rule = " ".join(link_rule.split(" ")[2:])
 
-# link test executable 
-os.system("${FC} ${FFLAGS} -I${OBJ_DIR} ${INC} " 
-           + test_obj + " " + link_rule + " -o " + test_exe + " ${LIB}")
+print("TEST BUILDER: linking test executable")
+sp.run("${FC} ${FFLAGS} -I${OBJ_DIR} ${INC} " + test_obj + " " + link_rule + " -o " + test_exe + " ${LIB}",
+       shell=True, check=True)
+
+print("TEST BUILDER: success")
