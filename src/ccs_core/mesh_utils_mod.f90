9cfdf57c5b96544f8e55d70c3c6178274f371d50
Michele Weiland <m.weiland@epcc.ed.ac.uk>
2022-02-04 13:47:04 +0000

    Revert

--- src/ccs_core/mesh_utils_mod.f90
+++ src/ccs_core/mesh_utils_mod.f90
@@ -67,7 +67,6 @@ contains
           !! Get neighbour indices
           !! XXX: These are global indices and thus may be off-process
           ictr = 1
-!$omp simd lastprivate(xc)
           do i = istart, iend
             square_mesh%idx_global(ictr) = i
             ii = i - 1
