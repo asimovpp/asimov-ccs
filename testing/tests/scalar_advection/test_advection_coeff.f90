da718e933d0c7d11efa852e8700c2e641340728c
Alexei <a.borissov@epcc.ed.ac.uk>
2022-03-02 15:49:09 +0000

    Fix advection coefficients test

--- testing/tests/scalar_advection/test_advection_coeff.f90
+++ testing/tests/scalar_advection/test_advection_coeff.f90
@@ -6,24 +6,31 @@
 program test_advection_coeff
 
   use testing_lib
-  use constants, only: ndim
+  use constants, only: ndim, insert_mode
   use types, only: field, upwind_field, central_field, cell_locator, face_locator, neighbour_locator
   use mesh_utils, only : build_square_mesh
+  use vec, only : create_vector
   use fv, only: calc_advection_coeff, calc_cell_coords
   use meshing, only: set_cell_location, set_face_location, set_neighbour_location, &
                      get_global_index, get_face_area, get_face_normal
+  use utils, only : update, initialise, &
+                set_global_size, pack_entries, set_values
+  use petsctypes, only: vector_petsc
+  use petscvec
 
   type(mesh) :: square_mesh
+  type(vector_init_data) :: vec_sizes
+  class(field), allocatable :: scalar
   class(field), allocatable :: u, v
-  integer(accs_int), parameter :: cps = 50
+  integer(accs_int), parameter :: cps = 5
   integer(accs_int) :: self_idx, ngb_idx, local_idx
   integer(accs_int) :: ngb
   integer(accs_int) :: direction, discretisation
-  integer(accs_int) :: row, col
   integer, parameter :: x_dir = 1, y_dir = 2
   integer, parameter :: central = -1, upwind = -2
   real(accs_real) :: face_area
   real(accs_real), dimension(ndim) :: normal
+  real(accs_real), dimension(:), pointer :: u_data, v_data
   
   call init()
 
@@ -32,13 +39,67 @@ program test_advection_coeff
   local_idx = int(0.5*square_mesh%nlocal + 2, accs_int)
   do direction = x_dir, y_dir
     do discretisation = upwind, central
-      call set_velocity_fields(direction, cps, discretisation, u, v)
+      if (discretisation == central) then
+        allocate(central_field :: scalar)
+        allocate(central_field :: u)
+        allocate(central_field :: v)
+      else if (discretisation == upwind) then
+        allocate(upwind_field :: scalar)
+        allocate(upwind_field :: u)
+        allocate(upwind_field :: v)
+      else
+        write(message, *) 'Invalid discretisation type selected'
+        call stop_test(message)
+      end if
+
+      call initialise(vec_sizes)
+      call set_global_size(vec_sizes, square_mesh%nglobal, par_env)
+      call create_vector(vec_sizes, scalar%vec)
+      call create_vector(vec_sizes, u%vec)
+      call create_vector(vec_sizes, v%vec)
+
+      call set_velocity_fields(square_mesh, direction, u, v)
+
+      associate (u_vec => u%vec, v_vec => v%vec)
+      select type(u_vec)
+        type is(vector_petsc)
+          call VecGetArrayF90(u_vec%v, u_data, ierr)
+        class default
+          print *, 'invalid vector type'
+          stop
+      end select
+
+      select type(v_vec)
+        type is(vector_petsc)
+          call VecGetArrayF90(v_vec%v, v_data, ierr)
+        class default
+          print *, 'invalid vector type'
+          stop
+      end select
+
       do ngb = 1, 4
         call get_cell_parameters(local_idx, ngb, self_idx, ngb_idx, face_area, normal)
-        call calc_cell_coords(self_idx, cps, row, col)
-        call run_advection_coeff_test(u, v, self_idx, row, col, ngb_idx, face_area, normal)
+        call run_advection_coeff_test(scalar, u_data, v_data, self_idx, ngb_idx, face_area, normal)
       end do
-      call tidy_velocity_fields(u, v)
+
+      select type(u_vec)
+        type is(vector_petsc)
+          call VecRestoreArrayF90(u_vec%v, u_data, ierr)
+        class default
+          print *, 'invalid vector type'
+          stop
+      end select
+
+      select type(v_vec)
+        type is(vector_petsc)
+          call VecRestoreArrayF90(v_vec%v, v_data, ierr)
+        class default
+          print *, 'invalid vector type'
+          stop
+      end select
+      end associate
+
+      call tidy_velocity_fields(scalar, u, v)
     end do
   end do
 
@@ -85,40 +146,55 @@ program test_advection_coeff
   !> @param[in] cps            - Number of cells per side in (square) mesh
   !> @param[in] discretisation - Integer indicating which discretisation scheme to use
   !> @param[out] u, v          - The velocity fields in x and y directions
-  subroutine set_velocity_fields(direction, cps, discretisation, u, v)
+  subroutine set_velocity_fields(cell_mesh, direction, u, v)
+    class(mesh), intent(in) :: cell_mesh
     integer(accs_int), intent(in) :: direction
-    integer(accs_int), intent(in) :: cps
-    integer(accs_int), intent(in) :: discretisation
     class(field), intent(inout), allocatable :: u, v
+    type(cell_locator) :: self_loc
+    type(vector_values) :: u_vals, v_vals
+    integer(accs_int) :: local_idx, self_idx
+    real(accs_real) :: u_val, v_val
 
-    if (discretisation == central) then
-      allocate(central_field :: u)
-      allocate(central_field :: v)
-    else if (discretisation == upwind) then
-      allocate(upwind_field :: u)
-      allocate(upwind_field :: v)
-    else
-      write(message, *) 'Invalid discretisation type selected'
-      call stop_test(message)
-    end if
-    allocate(u%val(cps,cps))
-    allocate(v%val(cps,cps))
+    u_vals%mode = insert_mode
+    v_vals%mode = insert_mode
+    
+    associate(n_local => cell_mesh%nlocal)
+      allocate(u_vals%idx(n_local))
+      allocate(v_vals%idx(n_local))
+      allocate(u_vals%val(n_local))
+      allocate(v_vals%val(n_local))
+      
+      ! Set IC velocity fields
+      do local_idx = 1, n_local
+        call set_cell_location(self_loc, cell_mesh, local_idx)
+        call get_global_index(self_loc, self_idx)
 
-    if (direction == x_dir) then
-       u%val(:,:) = 1.0_accs_real
-       v%val(:,:) = 0.0_accs_real
-    else if (direction == y_dir) then
-      u%val(:,:) = 0.0_accs_real
-      v%val(:,:) = 1.0_accs_real
-    end if
+        if (direction == x_dir) then
+          u_val = 1.0_accs_real
+          v_val = 0.0_accs_real
+        else if (direction == y_dir) then
+          u_val = 0.0_accs_real
+          v_val = 1.0_accs_real
+        end if
+
+        call pack_entries(u_vals, local_idx, self_idx, u_val)
+        call pack_entries(v_vals, local_idx, self_idx, v_val)
+      end do
+    end associate
+    call set_values(u_vals, u%vec)
+    call set_values(v_vals, v%vec)
+
+    deallocate(u_vals%idx, v_vals%idx, u_vals%val, v_vals%val)
   end subroutine set_velocity_fields
 
   !> @brief Deallocates the velocity fields
   !
   !> @param[in] u, v - The velocity fields to deallocate
-  subroutine tidy_velocity_fields(u, v)
+  subroutine tidy_velocity_fields(scalar, u, v)
+    class(field), allocatable :: scalar
     class(field), allocatable :: u, v
 
+    deallocate(scalar)
     deallocate(u)
     deallocate(v)
   end subroutine tidy_velocity_fields
@@ -127,65 +203,51 @@ program test_advection_coeff
   !
   !> @param[in] u, v              - The velocity fields
   !> @param[in] self_idx          - The given cell's global index
-  !> @param[in] row               - The given cell's row in spatial mesh
-  !> @param[in] col               - The given cell's column in spatial mesh
   !> @param[in] ngb_idx           - The neighbour's global index
   !> @param[in] face_area - The surface area of the face between the cell and neighbour
   !> @param[in] normal            - The normal to the face between the cell and neighbour
-  subroutine run_advection_coeff_test(u, v, self_idx, row, col, ngb_idx, face_area, normal)
-    class(field), intent(in) :: u, v
+  subroutine run_advection_coeff_test(phi, u, v, self_idx, ngb_idx, face_area, face_normal)
+    class(field), intent(in) :: phi
+    real(accs_real), dimension(:), intent(in) :: u, v
     integer(accs_int), intent(in) :: self_idx
-    integer(accs_int), intent(in) :: row, col
     integer(accs_int), intent(in) :: ngb_idx
     real(accs_real), intent(in) :: face_area
-    real(accs_real), dimension(ndim), intent(in) :: normal
+    real(accs_real), dimension(ndim), intent(in) :: face_normal
 
     real(accs_real) :: coeff
     real(accs_real) :: expected_coeff
 
-    select type(u)
+    select type(phi)
       type is(central_field)
-        select type(v)
-          type is(central_field)
-            call calc_advection_coeff(self_idx, ngb_idx, face_area, cps, u, v, 0_accs_int, coeff)
-          class default
-            write(message, *) "FAIL: incorrect velocity field discretisation"
-            call stop_test(message)
-        end select
+        call calc_advection_coeff(phi, ngb_idx, self_idx, face_area, face_normal, u, v, 0_accs_int, coeff)
       type is(upwind_field)
-        select type(v)
-          type is(upwind_field)
-            call calc_advection_coeff(self_idx, ngb_idx, face_area, cps, u, v, 0_accs_int, coeff)
-          class default
-            write(message, *) "FAIL: incorrect velocity field discretisation"
-            call stop_test(message)
-        end select
+        call calc_advection_coeff(phi, ngb_idx, self_idx, face_area, face_normal, u, v, 0_accs_int, coeff)
       class default
         write(message, *) "FAIL: incorrect velocity field discretisation"
         call stop_test(message)
     end select
 
-    select type(u)
+    select type(phi)
       type is(upwind_field)
-        expected_coeff = min(-face_area*(u%val(col,row)*normal(1) + v%val(col,row)*normal(2)), 0.0_accs_real)
+        expected_coeff = min(face_area*(u(self_idx)*normal(1) + v(self_idx)*normal(2)), 0.0_accs_real)
       type is(central_field)
-        expected_coeff = -face_area*0.5_accs_real*(u%val(col,row)*normal(1) + v%val(col,row)*normal(2))
+        expected_coeff = face_area*0.5_accs_real*(u(self_idx)*normal(1) + v(self_idx)*normal(2))
       class default
         write(message, *) "FAIL: incorrect velocity field discretisation"
         call stop_test(message)
     end select
         
-    select type(u)
+    select type(phi)
       type is(upwind_field)
         if (abs(coeff - expected_coeff) .ge. eps) then
           write(message, *) "FAIL: incorrect upwind advection coefficient computed. Expected ", expected_coeff, " computed ", &
-                            coeff, " normal ", normal, " u ", u%val(col, row), " v ", v%val(col, row)
+                            coeff, " normal ", normal, " u ", u(self_idx), " v ", v(self_idx)
           call stop_test(message)
         end if
       type is(central_field)
         if (abs(coeff - expected_coeff) .ge. eps) then
           write(message, *) "FAIL: incorrect central advection coefficient computed. Expected ", expected_coeff, " computed ", &
-                            coeff, " normal ", normal, " u ", u%val(col, row), " v ", v%val(col, row)
+                            coeff, " normal ", normal, " u ", u(self_idx), " v ", v(self_idx)
           call stop_test(message)
         end if
       class default
