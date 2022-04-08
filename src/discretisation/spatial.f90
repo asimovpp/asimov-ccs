c8757e56bf052a167afc6ab480795107302b2080
Alexei Borissov <alebori3@gmail.com>
2022-04-08 16:47:09 +0100

    remove unnessary use statements in spatial.f90

--- src/discretisation/spatial.f90
+++ src/discretisation/spatial.f90
@@ -14,14 +14,12 @@ pure subroutine calc_adv_coeffs(coeffL, coeffR, advvel)
   !! + coeffL - the coefficient for the contribution to the "left" of the face
   !! + coeffR - the coefficient for the contribution to the "right" of the face
 
-  use constants
-  
   implicit none
 
   real(accs_real), intent(in) :: advvel
   real(accs_real), intent(out) :: coeffL, coeffR
 
-  coeffL = 0.5 * advvel
+  coeffL = 0.5_accs_real * advvel
   coeffR = coeffL
   
 end subroutine calc_adv_coeffs
