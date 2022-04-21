c7f053c9fe38a7e9709c230d1cef52cdb5c20057
Chris Goddard <Christopher.Goddard@rolls-royce.com>
2022-04-21 16:50:25 +0100

    Adding global_ prefix to 'start' variable in io routines

--- src/io/adios2/io_adios2.f90
+++ src/io/adios2/io_adios2.f90
@@ -189,13 +189,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of integer array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 1D integer array
-    module subroutine read_array_int32_1D(io_proc, var_name, start, count, var)
+    module subroutine read_array_int32_1D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(1), intent(in) :: start
+      integer(int64), dimension(1), intent(in) :: global_start
       integer(int64), dimension(1), intent(in) :: count
       integer(int32), dimension(:), intent(inout) :: var
 
@@ -207,7 +207,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 1, start, count, ierr)
+          call adios2_set_selection(adios2_var, 1, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_integer8) then
 
@@ -240,13 +240,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of integer array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 1D integer array    
-    module subroutine read_array_int64_1D(io_proc, var_name, start, count, var)
+    module subroutine read_array_int64_1D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(1), intent(in) :: start
+      integer(int64), dimension(1), intent(in) :: global_start
       integer(int64), dimension(1), intent(in) :: count
       integer(int64), dimension(:), intent(inout) :: var
 
@@ -258,7 +258,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 1, start, count, ierr)
+          call adios2_set_selection(adios2_var, 1, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_integer4) then
 
@@ -290,13 +290,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of integer array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start    : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 2D integer array
-    module subroutine read_array_int32_2D(io_proc, var_name, start, count, var)
+    module subroutine read_array_int32_2D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(2), intent(in) :: start
+      integer(int64), dimension(2), intent(in) :: global_start
       integer(int64), dimension(2), intent(in) :: count
       integer(int32), dimension(:,:), intent(inout) :: var
 
@@ -308,7 +308,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 2, start, count, ierr)
+          call adios2_set_selection(adios2_var, 2, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_integer8) then
 
@@ -341,13 +341,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of integer array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 2D integer array
-    module subroutine read_array_int64_2D(io_proc, var_name, start, count, var)
+    module subroutine read_array_int64_2D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(2), intent(in) :: start
+      integer(int64), dimension(2), intent(in) :: global_start
       integer(int64), dimension(2), intent(in) :: count
       integer(int64), dimension(:,:), intent(inout) :: var
 
@@ -359,7 +359,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 2, start, count, ierr)
+          call adios2_set_selection(adios2_var, 2, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_integer4) then
 
@@ -391,13 +391,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of real array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 1D real array
-    module subroutine read_array_real32_1D(io_proc, var_name, start, count, var)
+    module subroutine read_array_real32_1D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(1), intent(in) :: start
+      integer(int64), dimension(1), intent(in) :: global_start
       integer(int64), dimension(1), intent(in) :: count
       real(real32), dimension(:), intent(inout) :: var
 
@@ -409,7 +409,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 1, start, count, ierr)
+          call adios2_set_selection(adios2_var, 1, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_dp) then
 
@@ -442,13 +442,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of real array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 1D real array
-    module subroutine read_array_real64_1D(io_proc, var_name, start, count, var)
+    module subroutine read_array_real64_1D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(1), intent(in) :: start
+      integer(int64), dimension(1), intent(in) :: global_start
       integer(int64), dimension(1), intent(in) :: count
       real(real64), dimension(:), intent(inout) :: var
 
@@ -460,7 +460,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 1, start, count, ierr)
+          call adios2_set_selection(adios2_var, 1, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_real) then
 
@@ -492,13 +492,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of real array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 2D real array
-    module subroutine read_array_real32_2D(io_proc, var_name, start, count, var)
+    module subroutine read_array_real32_2D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(2), intent(in) :: start
+      integer(int64), dimension(2), intent(in) :: global_start
       integer(int64), dimension(2), intent(in) :: count
       real(real32), dimension(:,:), intent(inout) :: var
 
@@ -510,7 +510,7 @@ submodule (io) io_adios2
         type is(adios2_io_process)
 
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
-          call adios2_set_selection(adios2_var, 2, start, count, ierr)
+          call adios2_set_selection(adios2_var, 2, global_start, count, ierr)
 
           if (adios2_var%type == adios2_type_dp) then
 
@@ -543,13 +543,13 @@ submodule (io) io_adios2
     !
     !> param[in]    io_proc  : ADIOS2 IO process used for reading
     !> param[in]    var_name : Name of real array to read
-    !> param[in]    start    : What global index to start reading from
+    !> param[in]    global_start : What global index to start reading from
     !> param[in]    count    : How many array element to read
     !> param[input] var      : The 2D real array
-        module subroutine read_array_real64_2D(io_proc, var_name, start, count, var)
+        module subroutine read_array_real64_2D(io_proc, var_name, global_start, count, var)
       class(io_process), intent(in) :: io_proc
       character (len=*), intent(in) :: var_name
-      integer(int64), dimension(2), intent(in) :: start
+      integer(int64), dimension(2), intent(in) :: global_start
       integer(int64), dimension(2), intent(in) :: count
       real(real64), dimension(:,:), intent(inout) :: var
 
@@ -563,7 +563,7 @@ submodule (io) io_adios2
           call adios2_inquire_variable(adios2_var, io_proc%io_task, var_name, ierr)
 
 
-          call adios2_set_selection(adios2_var, 2, start, count, ierr)
+          call adios2_set_selection(adios2_var, 2, global_start, count, ierr)
           if (adios2_var%type == adios2_type_real) then
 
             allocate(tmp_var32(size(var,dim=1),size(var,dim=2)))
