108e8934b5231ce2b3d05fa1e32a4610d1932388
Michele Weiland <m.weiland@epcc.ed.ac.uk>
2022-04-27 14:15:57 +0100

    Make example graph 1-indexed

--- tests/mesh/test_mesh_partitioning_parhip.f90
+++ tests/mesh/test_mesh_partitioning_parhip.f90
@@ -1,24 +1,23 @@
 !> Test that partitions a very simple graph using ParHIP
 !
-!  Sample graph
+!  Sample graph - adapted from ParMETIS manual to use 1-indexing
 ! 
-!   0 -- 1 -- 2 -- 3 -- 4
-!   |    |    |    |    |
-!   5 -- 6 -- 7 -- 8 -- 9
-!   |    |    |    |    |
-!  10 --11 --12 --13 --14
+!     1 -- 2 -- 3 -- 4 -- 5
+!     |    |    |    |    |
+!     6 -- 7 -- 8 -- 9 --10
+!     |    |    |    |    |
+!    11 --12 --13 --14 --15
 !
 
 program test_mesh_partitioning_parhip
 
   use testing_lib
   use partitioning, only: partition_kway
-  use kinds, only: ccs_int, ccs_idx
+  use kinds, only: ccs_int, ccs_long
   use types, only: topology
 
   type(topology) :: topo
-  integer(ccs_idx), dimension(:), allocatable :: partition_vector
-
+  integer(ccs_long), dimension(:), allocatable :: partition_vector
 
   implicit none
 
@@ -31,32 +30,32 @@ program test_mesh_partitioning_parhip
 
       if(par_env%proc_id == 0) then
         allocate(topo%xadj(6))
-        allocate(topo%vtxdist(4))
         allocate(topo%adjncy(13))
-        topo%xadj = (/ 0 2 5 8 11 13 /)
-        topo%vtxdist = (/ 0 5 10 15 /)
-        topo%adjncy = (/ 1 5 0 2 6 1 3 7 2 4 8 3 9 /)
+        allocate(topo%vtxdist(4))
+        topo%xadj = (/ 1 3 6 9 12 14 /)
+        topo%adjncy = (/ 2 6 1 3 7 2 4 8 3 5 9 4 10 /)
+        topo%vtxdist = (/ 1 6 11 16 /)
       else if (par_env%proc_id == 1) then
         allocate(topo%xadj(6))
-        allocate(topo%vtxdist(4))
         allocate(topo%adjncy(15))
-        topo%xadj = (/ 0 3 7 11 15 18 /)      
-        topo%vtxdist = (/ 0 5 10 15 /)
-        topo%adjncy = (/ 0 6 10 1 5 7 11 2 6 8 12 3 7 9 13 4 8 14 /)
+        allocate(topo%vtxdist(4))
+        topo%xadj = (/ 1 4 8 12 16 19 /)      
+        topo%adjncy = (/ 1 7 11 2 6 8 12 3 7 9 13 4 8 10 14 5 9 15 /)
+        topo%vtxdist = (/ 1 6 11 16 /)
       else 
         allocate(topo%xadj(6))
-        allocate(topo%vtxdist(4))
         allocate(topo%adjncy(13))
-        topo%vtxdist = (/ 0 5 10 15 /)
-        topo%xadj = (/ 0 2 5 8 11 13 /)
-        topo%adjncy = (/ 5 11 6 10 12 7 11 13 8 12 14 9 13 /)
+        allocate(topo%vtxdist(4))
+        topo%xadj = (/ 1 3 6 9 12 14 /)
+        topo%adjncy = (/ 6 12 7 11 13 8 12 14 9 13 15 10 14 /)
+        topo%vtxdist = (/ 1 6 11 16 /)
       end if
     
     else
       print*,"Test must be run on 3 MPI ranks"
     end if 
 
-    !call partition_kway(par_env, topo, partition_vector)
+    call partition_kway(par_env, topo, partition_vector)
 
   class default
     write(message, *) "ERROR: Unknown parallel environment!"
