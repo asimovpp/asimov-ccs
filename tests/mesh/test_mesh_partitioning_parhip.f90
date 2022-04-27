d0424601e354279e9ab2f7c5a067e622e2b7a432
Michele Weiland <m.weiland@epcc.ed.ac.uk>
2022-04-27 14:34:08 +0100

    Fix copy & paste typos in the partitioning test

--- tests/mesh/test_mesh_partitioning_parhip.f90
+++ tests/mesh/test_mesh_partitioning_parhip.f90
@@ -32,38 +32,46 @@ program test_mesh_partitioning_parhip
         allocate(topo%xadj(6))
         allocate(topo%adjncy(13))
         allocate(topo%vtxdist(4))
-        topo%xadj = (/ 1 3 6 9 12 14 /)
-        topo%adjncy = (/ 2 6 1 3 7 2 4 8 3 5 9 4 10 /)
-        topo%vtxdist = (/ 1 6 11 16 /)
+        topo%xadj = (/ 1, 3, 6, 9, 12, 14 /)
+        topo%adjncy = (/ 2, 6, 1, 3, 7, 2, 4, 8, 3, 5, 9, 4, 10 /)
+        topo%vtxdist = (/ 1, 6, 11, 16 /)
       else if (par_env%proc_id == 1) then
         allocate(topo%xadj(6))
         allocate(topo%adjncy(15))
         allocate(topo%vtxdist(4))
-        topo%xadj = (/ 1 4 8 12 16 19 /)      
-        topo%adjncy = (/ 1 7 11 2 6 8 12 3 7 9 13 4 8 10 14 5 9 15 /)
-        topo%vtxdist = (/ 1 6 11 16 /)
+        topo%xadj = (/ 1, 4, 8, 12, 16, 19 /)      
+        topo%adjncy = (/ 1, 7, 11, 2, 6, 8, 12, 3, 7, 9, 13, 4, 8, 10, 14, 5, 9, 15 /)
+        topo%vtxdist = (/ 1, 6, 11, 16 /)
       else 
         allocate(topo%xadj(6))
         allocate(topo%adjncy(13))
         allocate(topo%vtxdist(4))
-        topo%xadj = (/ 1 3 6 9 12 14 /)
-        topo%adjncy = (/ 6 12 7 11 13 8 12 14 9 13 15 10 14 /)
-        topo%vtxdist = (/ 1 6 11 16 /)
+        topo%xadj = (/ 1, 3, 6, 9, 12, 14 /)
+        topo%adjncy = (/ 6, 12, 7, 11, 13, 8, 12, 14, 9, 13, 15, 10, 14 /)
+        topo%vtxdist = (/ 1, 6, 11, 16 /)
       end if
     
     else
       print*,"Test must be run on 3 MPI ranks"
     end if 
 
-    call partition_kway(par_env, topo, partition_vector)
+    ! call partition_kway(par_env, topo, partition_vector)
 
   class default
     write(message, *) "ERROR: Unknown parallel environment!"
     call stop_test(message)
   end select
 
-  if(allocated(adjncy)) then
-    deallocate(adjncy)
+  if(allocated(topo%xadj)) then
+    deallocate(topo%xadj)
+  end if
+
+  if(allocated(topo%adjncy)) then
+    deallocate(topo%adjncy)
+  end if
+
+  if(allocated(topo%vtxdist)) then
+    deallocate(topo%vtxdist)
   end if
 
   call fin()
