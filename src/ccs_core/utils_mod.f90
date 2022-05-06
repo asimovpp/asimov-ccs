3f6369549f07c489669eced0cf0b2600a812a73c
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2022-05-06 15:35:15 +0100

    Rename error exit routine

--- src/ccs_core/utils_mod.f90
+++ src/ccs_core/utils_mod.f90
@@ -33,7 +33,7 @@ module utils
   public :: zero
   public :: str
   public :: debug_print
-  public :: err_exit
+  public :: exit_print 
 
   !>  Generic interface to set values on an object.
   interface set_values
@@ -180,14 +180,14 @@ contains
   end function
 
   !> Print a message and stop program execution.
-  subroutine err_exit(msg, filepath, line)
+  subroutine exit_print(msg, filepath, line)
     character(*), intent(in) :: msg      !< text to be printed
     character(*), intent(in) :: filepath !< calling file, added by __FILE__ macro
     integer, intent(in) :: line          !< line number in calling file, added by __LINE__ macro
 
     call debug_print(msg, filepath, line)
     stop 1
-  end subroutine err_exit
+  end subroutine exit_print
 
 
 
