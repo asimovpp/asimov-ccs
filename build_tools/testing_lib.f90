ae29bc53ae657d39225f671f5c64d14a705c8a89
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2022-08-05 17:22:47 +0100

    Add prototype asserts structure

--- build_tools/testing_lib.f90
+++ build_tools/testing_lib.f90
@@ -11,13 +11,28 @@ module testing_lib
 
   implicit none
 
+  public :: assert_eq
+
+  interface assert_eq
+    procedure assert_eq_integer_rank0
+    procedure assert_eq_integer_rank1
+    procedure assert_eq_real_rank0
+    procedure assert_eq_real_rank1
+    procedure assert_eq_string
+  end interface
+
+  interface a_eq
+    procedure a_eq_integer
+    procedure a_eq_real
+  end interface
+  
   class(parallel_environment), allocatable, target :: par_env
   integer(ccs_err) :: ierr
   integer :: real_type
   character(1024) :: message
 
   real(ccs_real), parameter :: eps = epsilon(0.0_ccs_real)
-  
+
 contains
 
   !>  Test initialisation
@@ -103,23 +118,110 @@ contains
 
     stop 1
   end subroutine stop_test
-  
+ 
+
+
   !>  Assertion for integer equality
   !
   !> @description Check whether input integers are equal. If not, construct message, print and stop.
-  subroutine assert_equal(a, b, msg_format)
+  subroutine assert_eq_integer_rank0(test_value, reference_value, msg_format)
 
-    integer(ccs_int), intent(in) :: a
-    integer(ccs_int), intent(in) :: b
-    character(*), intent(in) :: msg_format
+    integer(ccs_int), intent(in) :: test_value       !< Test value
+    integer(ccs_int), intent(in) :: reference_value  !< reference value
+    character(*), intent(in) :: msg_format           !< Error message 
     character(1024) :: message
 
-    if (a /= b) then
-      write (message, msg_format) a, b
+    if (.not. a_eq(test_value, reference_value)) then
+      write (message, msg_format) test_value, reference_value
       call stop_test(message)
     end if
 
-  end subroutine assert_equal
+  end subroutine assert_eq_integer_rank0
+  subroutine assert_eq_integer_rank1(test_value, reference_value, msg_format)
+
+    integer(ccs_int), dimension(:), intent(in) :: test_value       !< Test value
+    integer(ccs_int), dimension(:), intent(in) :: reference_value  !< reference value
+    character(*), intent(in) :: msg_format                         !< Error message 
+    character(1024) :: message
+
+    if (.not. all(a_eq(test_value, reference_value))) then
+      write (message, msg_format) test_value, reference_value
+      call stop_test(message)
+    end if
+
+  end subroutine assert_eq_integer_rank1
+ 
+
+
+  !>  assertion for real equality
+  !
+  !> @description check whether input reals are equal. if not, construct message, print and stop.
+  subroutine assert_eq_real_rank0(test_value, reference_value, msg_format)
+
+    real(ccs_real), intent(in) :: test_value      !< Test value
+    real(ccs_real), intent(in) :: reference_value !< reference value
+    character(*), intent(in) :: msg_format        !< Error message 
+    character(1024) :: message
+
+    if (.not. a_eq(test_value, reference_value)) then
+      write (message, msg_format) test_value, reference_value
+      call stop_test(message)
+    end if
+
+  end subroutine assert_eq_real_rank0
+  subroutine assert_eq_real_rank1(test_value, reference_value, msg_format)
+
+    real(ccs_real), dimension(:), intent(in) :: test_value      !< Test value
+    real(ccs_real), dimension(:), intent(in) :: reference_value !< reference value
+    character(*), intent(in) :: msg_format                      !< Error message 
+    character(1024) :: message
+
+    if (.not. all(a_eq(test_value, reference_value))) then
+      write (message, msg_format) test_value, reference_value
+      call stop_test(message)
+    end if
+
+  end subroutine assert_eq_real_rank1
+ 
+
+
+  !>  assertion for string equality
+  !
+  !> @description check whether input strings are equal. if not, construct message, print and stop.
+  subroutine assert_eq_string(test_value, reference_value, msg_format)
+
+    character(*), intent(in) :: test_value      !< Test value
+    character(*), intent(in) :: reference_value !< reference value
+    character(*), intent(in) :: msg_format      !< Error message 
+    character(1024) :: message
+
+    if (test_value /= reference_value) then
+      write (message, msg_format) test_value, reference_value
+      call stop_test(message)
+    end if
+
+  end subroutine assert_eq_string
+ 
+
+
+
+
   
+
+  elemental logical function a_eq_integer(a, b) result(comparison)
+    integer(ccs_int), intent(in) :: a 
+    integer(ccs_int), intent(in) :: b 
+
+    comparison = a == b
+  end function a_eq_integer
   
+  elemental logical function a_eq_real(a, b) result(comparison)
+    real(ccs_real), intent(in) :: a 
+    real(ccs_real), intent(in) :: b 
+
+    comparison = (abs(a - b) < epsilon(b) * abs(b))
+  end function a_eq_real
+
+
+
 end module testing_lib
