e719f3340234c39d24c830fcc23ab4be9c94b74e
paul <paul@ccs-ci>
2022-09-30 15:40:02 +0100

    Set TGV timestep based on CFL target

--- src/case_setup/TaylorGreenVortex/tgv2d.f90
+++ src/case_setup/TaylorGreenVortex/tgv2d.f90
@@ -58,6 +58,7 @@ program tgv2d
   real(ccs_real) :: dt       ! The timestep
   integer(ccs_int) :: t      ! Timestep counter
   integer(ccs_int) :: nsteps ! Number of timesteps to perform
+  real(ccs_real) :: CFL      ! The CFL target
   
 #ifndef EXCLUDE_MISSING_INTERFACE
   integer(ccs_int) :: ierr
@@ -164,7 +165,8 @@ program tgv2d
   ! Solve using SIMPLE algorithm
   if (irank == par_env%root) print *, "Start SIMPLE"
 
-  dt = 0.1_ccs_real
+  CFL = 0.1_ccs_real
+  dt = CFL * (3.14_ccs_real / cps)
   nsteps = 5
 
   call activate_timestepping()
