61501ab4db51018c0e9c4d629c1bf53afcd8909b
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2023-02-19 21:59:06 +0000

    Initial, crude modularisation of Poisson problem

--- src/case_setup/Poisson/poisson.f90
+++ src/case_setup/Poisson/poisson.f90
@@ -12,141 +12,35 @@
 !    p\left(\boldsymbol{x}\right) = y,\ \boldsymbol{x}\in\partial\Omega
 !  \]
 
-program poisson
+module poisson_discretisation
 
-  ! ASiMoV-CCS uses
-  use constants, only: ndim, add_mode, insert_mode
-  use kinds, only: ccs_real, ccs_int
-  use case_config, only: velocity_solver_method_name, velocity_solver_precon_name, &
-                         pressure_solver_method_name, pressure_solver_precon_name
-  use types, only: vector_spec, ccs_vector, matrix_spec, ccs_matrix, &
-                   equation_system, linear_solver, ccs_mesh, cell_locator, face_locator, &
-                   neighbour_locator, vector_values, matrix_values, matrix_values_spec
-  use meshing, only: set_cell_location, set_face_location, set_neighbour_location, get_local_num_cells
-  use vec, only: create_vector
-  use mat, only: create_matrix, set_nnz, create_matrix_values, set_matrix_values_spec_nrows, &
-                 set_matrix_values_spec_ncols
-  use solver, only: create_solver, solve, set_equation_system, axpy, norm, &
-       set_solver_method, set_solver_precon
-  use utils, only: update, begin_update, end_update, finalise, initialise, &
-                   set_size, &
-                   set_values, clear_entries, set_values, set_row, set_col, set_entry, set_mode
-  use vec, only: create_vector_values
-  use mesh_utils, only: build_square_mesh
-  use meshing, only: get_face_area, get_centre, get_volume, get_global_index, &
-                     count_neighbours, get_boundary_status
-  use parallel_types, only: parallel_environment
-  use parallel, only: initialise_parallel_environment, &
-                      cleanup_parallel_environment, &
-                      read_command_line_arguments, &
-                      timer, sync
+  use constants, only : ndim, add_mode, insert_mode
+  use kinds, only : ccs_int, ccs_real
+  use types, only : ccs_vector, vector_values, &
+       ccs_matrix, matrix_values_spec, matrix_values, &
+       ccs_mesh, cell_locator, neighbour_locator, face_locator
 
+  use mat, only : create_matrix_values, set_matrix_values_spec_ncols, set_matrix_values_spec_nrows
+  use meshing, only : get_local_num_cells, set_cell_location, get_centre, get_volume, &
+       get_global_index, count_neighbours, set_neighbour_location, get_boundary_status, &
+       set_face_location, get_face_area
+  use utils, only : clear_entries, set_mode, set_col, set_row, set_entry, set_values
+  use vec, only : create_vector_values
+  
   implicit none
 
-  class(parallel_environment), allocatable, target :: par_env
-  class(ccs_vector), allocatable, target :: u, b
-  class(ccs_vector), allocatable :: u_exact
-  class(ccs_matrix), allocatable, target :: M
-  class(linear_solver), allocatable :: poisson_solver
+  private
 
-  type(vector_spec) :: vec_properties
-  type(matrix_spec) :: mat_properties
-  type(equation_system) :: poisson_eq
-  type(ccs_mesh) :: mesh
-
-  integer(ccs_int) :: cps = 10 !< Default value for cells per side
-
-  real(ccs_real) :: err_norm
-
-  double precision :: start_time
-  double precision :: end_time
-
-  call initialise_parallel_environment(par_env)
-  call read_command_line_arguments(par_env, cps=cps)
-  
-  ! set solver and preconditioner info
-  velocity_solver_method_name = "gmres"
-  velocity_solver_precon_name = "bjacobi"
-  pressure_solver_method_name = "cg"
-  pressure_solver_precon_name = "gamg"
-
-  call sync(par_env)
-  call timer(start_time)
-
-  call initialise_poisson(par_env)
-
-  ! Initialise with default values
-  call initialise(vec_properties)
-  call initialise(mat_properties)
-  call initialise(poisson_eq)
-
-  ! Create stiffness matrix
-  call set_size(par_env, mesh, mat_properties)
-  call set_nnz(5, mat_properties)
-  call create_matrix(mat_properties, M)
-
-  call discretise_poisson(M)
-
-  call begin_update(M) ! Start the parallel assembly for M
-
-  ! Create right-hand-side and solution vectors
-  call set_size(par_env, mesh, vec_properties)
-  call create_vector(vec_properties, b)
-  call create_vector(vec_properties, u_exact)
-  call create_vector(vec_properties, u)
-
-  call begin_update(u) ! Start the parallel assembly for u
-
-  ! Evaluate right-hand-side vector
-  call eval_rhs(b)
-
-  call begin_update(b) ! Start the parallel assembly for b
-  call end_update(M) ! Complete the parallel assembly for M
-  call end_update(b) ! Complete the parallel assembly for b
-
-  ! Modify matrix and right-hand-side vector to apply Dirichlet boundary conditions
-  call apply_dirichlet_bcs(M, b)
-  call begin_update(b) ! Start the parallel assembly for b
-  call finalise(M)
-
-  call end_update(u) ! Complete the parallel assembly for u
-  call end_update(b) ! Complete the parallel assembly for b
-
-  ! Create linear solver & set options
-  call set_equation_system(par_env, b, u, M, poisson_eq)
-  call create_solver(poisson_eq, poisson_solver)
-  call set_solver_method(pressure_solver_method_name, poisson_solver)
-  call set_solver_precon(pressure_solver_precon_name, poisson_solver)
-  call solve(poisson_solver)
-
-  ! Check solution
-  call set_exact_sol(u_exact)
-  call axpy(-1.0_ccs_real, u_exact, u)
-
-  err_norm = norm(u, 2) * mesh%geo%h
-  if (par_env%proc_id == par_env%root) then
-    print *, "Norm of error = ", err_norm
-  end if
-
-  ! Clean up
-  deallocate (u)
-  deallocate (b)
-  deallocate (u_exact)
-  deallocate (M)
-  deallocate (poisson_solver)
-
-  call timer(end_time)
-
-  if (par_env%proc_id == par_env%root) then
-    print *, "Elapsed time = ", (end_time - start_time)
-  end if
-
-  call cleanup_parallel_environment(par_env)
+  public :: discretise_poisson
+  public :: apply_dirichlet_bcs
+  public :: eval_rhs
+  public :: rhs_val
 
 contains
 
-  subroutine eval_rhs(b)
+  subroutine eval_rhs(mesh, b)
 
+    type(ccs_mesh), intent(in) :: mesh
     class(ccs_vector), intent(inout) :: b
 
     integer(ccs_int) :: nloc
@@ -203,8 +97,9 @@ contains
 
   end subroutine eval_cell_rhs
 
-  subroutine discretise_poisson(M)
+  subroutine discretise_poisson(mesh, M)
 
+    type(ccs_mesh), intent(in) :: mesh
     class(ccs_matrix), intent(inout) :: M
 
     type(matrix_values_spec) :: mat_val_spec
@@ -286,10 +181,11 @@ contains
 
   end subroutine discretise_poisson
 
-  subroutine apply_dirichlet_bcs(M, b)
+  subroutine apply_dirichlet_bcs(mesh, M, b)
 
     implicit none
 
+    type(ccs_mesh), intent(in) :: mesh
     class(ccs_matrix), intent(inout) :: M
     class(ccs_vector), intent(inout) :: b
 
@@ -349,7 +245,7 @@ contains
             call set_face_location(mesh, i, j, loc_f)
             call get_face_area(loc_f, A)
             boundary_coeff = (2.0 / mesh%geo%h) * A
-            boundary_val = rhs_val(i, j)
+            boundary_val = rhs_val(mesh, i, j)
 
             ! Coefficient
             coeff = coeff - boundary_coeff
@@ -378,47 +274,9 @@ contains
 
   end subroutine apply_dirichlet_bcs
 
-  subroutine set_exact_sol(u_exact)
+  function rhs_val(mesh, i, f) result(r)
 
-    class(ccs_vector), intent(inout) :: u_exact
-
-    type(vector_values) :: vec_values
-    integer(ccs_int) :: i, local_num_cells
-
-    type(cell_locator) :: loc_p
-    integer(ccs_int) :: global_index_p
-    integer(ccs_int) :: nrows_working_set
-
-    nrows_working_set = 1_ccs_int
-    call create_vector_values(nrows_working_set, vec_values)
-    call set_mode(insert_mode, vec_values)
-
-    call get_local_num_cells(mesh, local_num_cells)
-    do i = 1, local_num_cells
-      call clear_entries(vec_values)
-      call set_cell_location(mesh, i, loc_p)
-      call get_global_index(loc_p, global_index_p)
-
-      call set_row(global_index_p, vec_values)
-      call set_entry(rhs_val(i), vec_values)
-      call set_values(vec_values, u_exact)
-    end do
-    deallocate (vec_values%global_indices)
-    deallocate (vec_values%values)
-
-    call update(u_exact)
-  end subroutine set_exact_sol
-
-  subroutine initialise_poisson(par_env)
-
-    class(parallel_environment) :: par_env
-
-    mesh = build_square_mesh(par_env, cps, 1.0_ccs_real)
-
-  end subroutine initialise_poisson
-
-  function rhs_val(i, f) result(r)
-
+    type(ccs_mesh), intent(in) :: mesh
     integer(ccs_int), intent(in) :: i !< Cell index
     integer(ccs_int), intent(in), optional :: f !< Face index (local wrt cell)
 
@@ -445,5 +303,181 @@ contains
     end if
 
   end function rhs_val
+  
+end module poisson_discretisation
+
+program poisson
+
+  use poisson_discretisation
+  
+  ! ASiMoV-CCS uses
+  use constants, only: ndim, add_mode, insert_mode
+  use kinds, only: ccs_real, ccs_int
+  use case_config, only: velocity_solver_method_name, velocity_solver_precon_name, &
+                         pressure_solver_method_name, pressure_solver_precon_name
+  use types, only: vector_spec, ccs_vector, matrix_spec, ccs_matrix, &
+                   equation_system, linear_solver, ccs_mesh, cell_locator, face_locator, &
+                   neighbour_locator, vector_values, matrix_values, matrix_values_spec
+  use meshing, only: set_cell_location, set_face_location, set_neighbour_location, get_local_num_cells
+  use vec, only: create_vector
+  use mat, only: create_matrix, set_nnz, create_matrix_values, set_matrix_values_spec_nrows, &
+                 set_matrix_values_spec_ncols
+  use solver, only: create_solver, solve, set_equation_system, axpy, norm, &
+       set_solver_method, set_solver_precon
+  use utils, only: update, begin_update, end_update, finalise, initialise, &
+                   set_size, &
+                   set_values, clear_entries, set_values, set_row, set_col, set_entry, set_mode
+  use vec, only: create_vector_values
+  use mesh_utils, only: build_square_mesh
+  use meshing, only: get_face_area, get_centre, get_volume, get_global_index, &
+                     count_neighbours, get_boundary_status
+  use parallel_types, only: parallel_environment
+  use parallel, only: initialise_parallel_environment, &
+                      cleanup_parallel_environment, &
+                      read_command_line_arguments, &
+                      timer, sync
+
+  implicit none
+
+  class(parallel_environment), allocatable, target :: par_env
+  class(ccs_vector), allocatable, target :: u, b
+  class(ccs_vector), allocatable :: u_exact
+  class(ccs_matrix), allocatable, target :: M
+  class(linear_solver), allocatable :: poisson_solver
+
+  type(vector_spec) :: vec_properties
+  type(matrix_spec) :: mat_properties
+  type(equation_system) :: poisson_eq
+  type(ccs_mesh) :: mesh
+
+  integer(ccs_int) :: cps = 10 !< Default value for cells per side
+
+  real(ccs_real) :: err_norm
+
+  double precision :: start_time
+  double precision :: end_time
+
+  call initialise_parallel_environment(par_env)
+  call read_command_line_arguments(par_env, cps=cps)
+  
+  ! set solver and preconditioner info
+  velocity_solver_method_name = "gmres"
+  velocity_solver_precon_name = "bjacobi"
+  pressure_solver_method_name = "cg"
+  pressure_solver_precon_name = "gamg"
+
+  call sync(par_env)
+  call timer(start_time)
+
+  call initialise_poisson(par_env)
+
+  ! Initialise with default values
+  call initialise(vec_properties)
+  call initialise(mat_properties)
+  call initialise(poisson_eq)
+
+  ! Create stiffness matrix
+  call set_size(par_env, mesh, mat_properties)
+  call set_nnz(5, mat_properties)
+  call create_matrix(mat_properties, M)
+
+  call discretise_poisson(mesh, M)
+
+  call begin_update(M) ! Start the parallel assembly for M
+
+  ! Create right-hand-side and solution vectors
+  call set_size(par_env, mesh, vec_properties)
+  call create_vector(vec_properties, b)
+  call create_vector(vec_properties, u_exact)
+  call create_vector(vec_properties, u)
+
+  call begin_update(u) ! Start the parallel assembly for u
+
+  ! Evaluate right-hand-side vector
+  call eval_rhs(mesh, b)
+
+  call begin_update(b) ! Start the parallel assembly for b
+  call end_update(M) ! Complete the parallel assembly for M
+  call end_update(b) ! Complete the parallel assembly for b
+
+  ! Modify matrix and right-hand-side vector to apply Dirichlet boundary conditions
+  call apply_dirichlet_bcs(mesh, M, b)
+  call begin_update(b) ! Start the parallel assembly for b
+  call finalise(M)
+
+  call end_update(u) ! Complete the parallel assembly for u
+  call end_update(b) ! Complete the parallel assembly for b
+
+  ! Create linear solver & set options
+  call set_equation_system(par_env, b, u, M, poisson_eq)
+  call create_solver(poisson_eq, poisson_solver)
+  call set_solver_method(pressure_solver_method_name, poisson_solver)
+  call set_solver_precon(pressure_solver_precon_name, poisson_solver)
+  call solve(poisson_solver)
+
+  ! Check solution
+  call set_exact_sol(u_exact)
+  call axpy(-1.0_ccs_real, u_exact, u)
+
+  err_norm = norm(u, 2) * mesh%geo%h
+  if (par_env%proc_id == par_env%root) then
+    print *, "Norm of error = ", err_norm
+  end if
+
+  ! Clean up
+  deallocate (u)
+  deallocate (b)
+  deallocate (u_exact)
+  deallocate (M)
+  deallocate (poisson_solver)
+
+  call timer(end_time)
+
+  if (par_env%proc_id == par_env%root) then
+    print *, "Elapsed time = ", (end_time - start_time)
+  end if
+
+  call cleanup_parallel_environment(par_env)
+
+contains
+
+  subroutine set_exact_sol(u_exact)
+
+    class(ccs_vector), intent(inout) :: u_exact
+
+    type(vector_values) :: vec_values
+    integer(ccs_int) :: i, local_num_cells
+
+    type(cell_locator) :: loc_p
+    integer(ccs_int) :: global_index_p
+    integer(ccs_int) :: nrows_working_set
+
+    nrows_working_set = 1_ccs_int
+    call create_vector_values(nrows_working_set, vec_values)
+    call set_mode(insert_mode, vec_values)
+
+    call get_local_num_cells(mesh, local_num_cells)
+    do i = 1, local_num_cells
+      call clear_entries(vec_values)
+      call set_cell_location(mesh, i, loc_p)
+      call get_global_index(loc_p, global_index_p)
+
+      call set_row(global_index_p, vec_values)
+      call set_entry(rhs_val(mesh, i), vec_values)
+      call set_values(vec_values, u_exact)
+    end do
+    deallocate (vec_values%global_indices)
+    deallocate (vec_values%values)
+
+    call update(u_exact)
+  end subroutine set_exact_sol
+
+  subroutine initialise_poisson(par_env)
+
+    class(parallel_environment) :: par_env
+
+    mesh = build_square_mesh(par_env, cps, 1.0_ccs_real)
+
+  end subroutine initialise_poisson
 
 end program poisson
