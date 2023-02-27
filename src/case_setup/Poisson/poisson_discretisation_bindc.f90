4e8aa3c6c035f46c710e879c3bbefbc103ffa064
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2023-02-27 13:49:06 +0000

    Fake a flat mesh structure for outlining to F/C/F

--- src/case_setup/Poisson/poisson_discretisation_bindc.f90
+++ src/case_setup/Poisson/poisson_discretisation_bindc.f90
@@ -1,6 +1,10 @@
 
 submodule (poisson_discretisation) poisson_discretisation_bindc
 
+  use iso_c_binding
+
+  use meshing, only: get_local_index
+  
   implicit none
   
 contains
@@ -18,19 +22,24 @@ contains
     integer(ccs_int) :: row, col
     real(ccs_real) :: coeff_f, coeff_p, coeff_nb
 
-    type(face_locator) :: loc_f
     real(ccs_real) :: A
 
     integer(ccs_int) :: global_index_p
     type(cell_locator) :: loc_p
     integer(ccs_int) :: nnb
 
-    type(neighbour_locator) :: loc_nb
-    logical :: is_boundary
     integer(ccs_int) :: global_index_nb
 
-    ! Loop over cells
+    integer(ccs_int), dimension(:), allocatable :: flat_neighbours
+    real(ccs_real), dimension(:), allocatable :: flat_areas
+
+    integer(ccs_int) :: index_nb
+    
+    ! Fake a flat mesh data structure
     call get_local_num_cells(mesh, local_num_cells)
+    call flatten_mesh(mesh, flat_neighbours, flat_areas)
+    
+    ! Loop over cells
     do i = 1, local_num_cells
       !^ @todo Doing this in a loop is awful code - malloc maximum coefficients per row once,
       !        filling from front, and pass the number of coefficients to be set, requires
@@ -50,18 +59,22 @@ contains
 
       ! Loop over faces
       do j = 1, nnb
-        call set_neighbour_location(loc_p, j, loc_nb)
-        call get_boundary_status(loc_nb, is_boundary)
+        ! call set_neighbour_location(loc_p, j, loc_nb)
+        ! call get_boundary_status(loc_nb, is_boundary)
 
-        if (.not. is_boundary) then
+        index_nb = flat_neighbours(4 * (i - 1) + j)
+        ! if (.not. is_boundary) then
+        if (index_nb > 0) then
           ! Interior face
 
-          call set_face_location(mesh, i, j, loc_f)
-          call get_face_area(loc_f, A)
+          ! call set_face_location(mesh, i, j, loc_f)
+          ! call get_face_area(loc_f, A)
+          A = flat_areas(4 * (i - 1) + j)
           coeff_f = (1.0 / mesh%geo%h) * A
 
-          call get_global_index(loc_nb, global_index_nb)
-
+          ! call get_global_index(loc_nb, global_index_nb)
+          global_index_nb = mesh%topo%global_indices(index_nb)
+          
           coeff_p = coeff_p - coeff_f
           coeff_nb = coeff_f
           col = global_index_nb
@@ -87,6 +100,9 @@ contains
 
     end do
 
+    deallocate(flat_neighbours)
+    deallocate(flat_areas)
+    
   end subroutine discretise_poisson
 
   module subroutine apply_dirichlet_bcs(mesh, M, b)
@@ -181,5 +197,49 @@ contains
     deallocate (vec_values%values)
 
   end subroutine apply_dirichlet_bcs
+
+  ! Fake a flat mesh data structure.
+  subroutine flatten_mesh(mesh, flat_neighbours, flat_areas)
+
+    type(ccs_mesh), intent(in) :: mesh
+    integer(ccs_int), dimension(:), allocatable, intent(out) :: flat_neighbours
+    real(ccs_real), dimension(:), allocatable, intent(out) :: flat_areas
+
+    integer(ccs_int) :: local_num_cells
+    integer(ccs_int) :: index_p
+    integer(ccs_int) :: index_nb
+
+    integer(ccs_int) :: j, ctr
+
+    type(cell_locator) :: loc_p
+    type(neighbour_locator) :: loc_nb
+    type(face_locator) :: loc_f
+
+    real(ccs_real) :: A
+    
+    call get_local_num_cells(mesh, local_num_cells)
+
+    allocate(flat_neighbours(4 * local_num_cells))
+    allocate(flat_areas(4 * local_num_cells))
+
+    ctr = 1
+    do index_p = 1, local_num_cells
+      call set_cell_location(mesh, index_p, loc_p)
+      
+      do j = 1, 4 ! XXX: Don't leave hardcoded!
+        call set_neighbour_location(loc_p, j, loc_nb)
+
+        call get_local_index(loc_nb, index_nb)
+        flat_neighbours(ctr) = index_nb
+        
+        call set_face_location(mesh, index_p, j, loc_f)
+        call get_face_area(loc_f, A)
+        flat_areas(ctr) = A
+
+        ctr = ctr + 1
+      end do
+    end do
+    
+  end subroutine flatten_mesh
   
 end submodule poisson_discretisation_bindc
