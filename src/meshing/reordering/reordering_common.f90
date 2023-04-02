e39285701ce0a8b28e57dd11b87ba54df339cbcd
paul <p.bartholomew@epcc.ed.ac.uk>
2023-04-02 21:32:21 +0100

    Replace hardcoded neighbour count in reordering code

--- src/meshing/reordering/reordering_common.f90
+++ src/meshing/reordering/reordering_common.f90
@@ -167,14 +167,19 @@ contains
     integer(ccs_int) :: idx_new
     
     integer(ccs_int), dimension(:, :), allocatable :: idx_nb
+
+    type(cell_locator) :: loc_p
+    integer(ccs_int) :: nnb
     
     call get_local_num_cells(mesh, local_num_cells)
 
-    allocate (idx_nb(4, local_num_cells))
+    allocate (idx_nb, mold=mesh%topo%nb_indices)
 
     idx_nb(:, :) = mesh%topo%nb_indices(:, :)
     do i = 1, local_num_cells ! First update the neighbour copy
-      do j = 1, 4
+      call set_cell_location(mesh, i, loc_p)
+      call count_neighbours(loc_p, nnb)
+      do j = 1, nnb
         idx_tmp = mesh%topo%nb_indices(j, i)
         if ((idx_tmp > 0) .and. (idx_tmp <= local_num_cells)) then
           idx_new = new_indices(idx_tmp)
