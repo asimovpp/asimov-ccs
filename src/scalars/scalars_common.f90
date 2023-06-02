dbf4a78ae4b490249ab8d0ba9c98d9d270ffdb0f
paul <p.bartholomew@epcc.ed.ac.uk>
2023-06-02 18:05:13 +0100

    Flesh out scalar solver

--- src/scalars/scalars_common.f90
+++ src/scalars/scalars_common.f90
@@ -4,18 +4,145 @@
 
 submodule(scalars) scalars_common
 
+  use constants, only: field_u, field_v, field_w, field_p, field_p_prime, field_mf
+
+  use kinds, only: ccs_int
+  use types, only: ccs_matrix, ccs_vector, &
+       vector_spec, matrix_spec, &
+       linear_solver, equation_system
+
+  use fv, only: compute_fluxes, update_gradient
+  use timestepping, only: update_old_values, get_current_step, apply_timestep
+
+  use vec, only: create_vector
+  use mat, only: create_matrix, set_nnz
+  use solver, only: create_solver, solve, set_equation_system
+
+  use meshing, only: get_max_faces
+  use utils, only: get_field, update, initialise, finalise, set_size
+  
+  logical, save :: first_call = .true.
+  integer(ccs_int), save :: previous_step = -1
+
+  !> List of fields not to be updated as transported scalars
+  integer(ccs_int), dimension(6), parameter :: skip_fields = &
+       (/ field_u, field_v, field_w, &
+          field_p, field_p_prime, &
+          field_mf /)
+
 contains
   
   !> Subroutine to perform scalar transport for all scalar fields.
   module subroutine calculate_scalars(par_env, mesh, flow)
-    class(parallel_environment), allocatable, intent(in) :: par_env   !< parallel environment
-    type(ccs_mesh), intent(in) :: mesh                                !< the mesh
-    type(fluid), intent(inout) :: flow                                !< The structure containting all the fluid fields
+    class(parallel_environment), allocatable, intent(in) :: par_env !< parallel environment
+    type(ccs_mesh), intent(in) :: mesh                              !< the mesh
+    type(fluid), intent(inout) :: flow                              !< The structure containting all the fluid fields
 
-    ! XXX: Temporarily silence unused variables
-    associate(foo => par_env, bar => mesh, baz => flow)
-    end associate
+    class(ccs_matrix), allocatable :: M
+    class(ccs_vector), allocatable :: rhs
+    class(ccs_vector), allocatable :: D
+    
+    integer(ccs_int) :: nfields  ! Number of variables in the flowfield
+    integer(ccs_int) :: s        ! Scalar field counter
+    class(field), pointer :: phi ! The scalar field
+
+    logical :: do_update
+    integer(ccs_int) :: current_step
+
+    type(vector_spec) :: vec_properties
+    type(matrix_spec) :: mat_properties
+
+    integer(ccs_int) :: max_faces
+    
+    ! Initialise equation system (reused across scalars)
+    call initialise(vec_properties)
+    call initialise(mat_properties)
+    call get_max_faces(mesh, max_faces)
+    call set_size(par_env, mesh, mat_properties)
+    call set_nnz(max_faces + 1, mat_properties)
+    call create_matrix(mat_properties, M)
+
+    call set_size(par_env, mesh, vec_properties)
+    call create_vector(vec_properties, rhs)
+    call create_vector(vec_properties, D)
+
+    ! Check whether we need to update the old values
+    do_update = .false.
+    
+    call get_current_step(current_step)
+    
+    if (first_call) then
+       first_call = .false.
+       do_update = .true.
+    else if (previous_step /= current_step) then
+       do_update = .true.
+    end if
+
+    previous_step = current_step
+
+    ! Transport the scalars
+    call count_fields(flow, nfields)
+    do s = 1, nfields
+       if (any(skip_fields == s)) then
+          continue
+       end if
+
+       call get_field(flow, s, phi)
+       if (do_update) then
+          call update_old_values(phi)
+       end if
+       call transport_scalar(par_env, mesh, flow, M, rhs, D, phi)
+    end do
     
   end subroutine calculate_scalars
+
+  !> Subroutine to transport a scalar field.
+  subroutine transport_scalar(par_env, mesh, flow, M, rhs, D, phi)
+
+    class(parallel_environment), allocatable, intent(in) :: par_env !< parallel environment
+    type(ccs_mesh), intent(in) :: mesh                              !< the mesh
+    type(fluid), intent(inout) :: flow                              !< The structure containting all the fluid fields
+    class(ccs_matrix), allocatable, intent(inout) :: M
+    class(ccs_vector), allocatable, intent(inout) :: rhs
+    class(ccs_vector), intent(inout) :: D
+    class(field), intent(inout) :: phi ! The scalar field
+
+    class(field), pointer :: mf  ! The advecting velocity field
+    class(linear_solver), allocatable :: lin_solver
+    type(equation_system) :: lin_system
+
+    call initialise(lin_system)
+
+    call update_gradient(mesh, phi)
+
+    call get_field(flow, field_mf, mf)
+    call compute_fluxes(phi, mf, mesh, 0, M, rhs)
+    call apply_timestep(mesh, phi, D, M, rhs)
+
+    call update(M)
+    call update(rhs)
+    call finalise(M)
+
+    if (allocated(phi%values%name)) then
+       call set_equation_system(par_env, rhs, phi%values, M, lin_system, phi%values%name)
+    else
+       call set_equation_system(par_env, rhs, phi%values, M, lin_system)
+    end if
+       
+    call create_solver(lin_system, lin_solver)
+    call solve(lin_solver)
+    deallocate(lin_solver)
+    
+  end subroutine transport_scalar
+
+  !> Get the count of stored fields - probably belongs somewhere else
+  subroutine count_fields(flow, nfields)
+
+    type(fluid), intent(in) :: flow          !< The flowfield
+    integer(ccs_int), intent(out) :: nfields !< The count of fields
+
+    nfields = size(flow%field_names)
+    
+  end subroutine count_fields
   
 end submodule scalars_common
