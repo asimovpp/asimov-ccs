e921854119e64df552f685cd569bdc49af2f14ec
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2023-07-17 17:18:18 +0100

    add test with disturbed cartesian mesh

---
+++ tests/nightly/verification/test_tgv_disturb_cartesian.f90
@@ -0,0 +1,106 @@
+!> @brief Test grid convergence of the 2d tgv test case that includes a prism layer
+!
+!> @description Generates several square meshes varying cps and get the error to compute their convergence order
+program test_tgv_disturb_cartesian
+#include "ccs_macros.inc"
+
+  use testing_lib
+  use error_analysis, only: get_order, print_error_summary
+  use mesh_utils, only: build_square_mesh
+  use tgv2d_core, only: run_tgv2d, domain_size
+  use mesh_utils, only: compute_face_interpolation
+  use meshing, only: get_local_num_cells
+
+  implicit none
+
+  type(ccs_mesh), target :: mesh
+  integer(ccs_int), parameter :: num_cps = 5
+  integer(ccs_int), parameter :: nvar = 3
+  real(ccs_real), dimension(nvar, num_cps) :: error_L2
+  real(ccs_real), dimension(nvar, num_cps) :: error_Linf
+  real(ccs_real), dimension(:), allocatable :: orders_L2
+  real(ccs_real), dimension(:), allocatable :: orders_Linf
+  real(ccs_real), dimension(num_cps) :: refinements
+  integer(ccs_int), dimension(num_cps) :: cps_list
+  integer(ccs_int) :: cps
+
+  character(len=12), dimension(nvar) :: variable_labels
+
+  integer(ccs_int) :: i, j
+
+  call init()
+
+  variable_labels = (/"U", "V", "P"/)
+  domain_size = 3.14159265358979323
+
+  cps_list = (/16, 32, 64, 128, 256/)
+  refinements = real(maxval(cps_list(:))) / real(cps_list(:))
+
+  error_L2(:, :) = 0.0_ccs_real
+  error_Linf(:, :) = 0.0_ccs_real
+
+  do i = 1, num_cps
+    cps = cps_list(i)
+    mesh = build_square_mesh(par_env, cps, domain_size)
+
+    call disturb_cartesian(cps, mesh)
+
+    call run_tgv2d(par_env, error_L2(:, i), error_Linf(:, i), mesh)
+  end do
+
+  if (par_env%proc_id == par_env%root) then
+
+    call print_error_summary(variable_labels, refinements, error_L2, error_Linf)
+
+    call get_order(refinements, error_L2, orders_L2)
+    call get_order(refinements, error_Linf, orders_Linf)
+
+    call assert_gt(orders_L2(1), 1.9_ccs_real, "U not converging in 2nd order ")
+    call assert_gt(orders_L2(2), 1.9_ccs_real, "V not converging in 2nd order ")
+    !call assert_gt(orders_L2(3), 1.9_ccs_real, "P not converging in 2nd order ")
+
+    call assert_gt(orders_Linf(1), 1.4_ccs_real, "U not converging in 2nd order ")
+    call assert_gt(orders_Linf(2), 1.4_ccs_real, "V not converging in 2nd order ")
+    !call assert_gt(orders_Linf(3), 1.4_ccs_real, "P not converging in 2nd order ")
+
+  end if
+
+  call fin()
+
+contains
+
+  !v Modifies a square mesh by randomly moving cell centres
+  subroutine disturb_cartesian(cps, mesh)
+    integer(ccs_int), intent(in) :: cps
+    type(ccs_mesh), intent(inout) :: mesh
+
+    real(ccs_real) :: dx
+    integer(ccs_int) :: icell, local_num_cells, idim
+    real(ccs_real) :: random_value
+    integer(ccs_int) :: n
+    integer(ccs_int), allocatable :: seed(:)
+    
+    call random_seed(size=n)
+    allocate(seed(n))
+    seed = 17
+    call random_seed(put=seed)
+    deallocate(seed)
+
+    dx = domain_size / real(cps)
+
+    call get_local_num_cells(mesh, local_num_cells)
+    do icell = 1, local_num_cells
+
+      do idim = 1, 2
+        call random_number(random_value) 
+        !random_value = 0.5_ccs_real
+        mesh%geo%x_p(idim, icell) = mesh%geo%x_p(idim, icell) + (random_value - 0.5_ccs_real)*dx/2.0
+      end do
+    end do
+
+    ! Update face interpolation
+    call compute_face_interpolation(mesh)
+
+  end subroutine
+
+end program test_tgv_disturb_cartesian
