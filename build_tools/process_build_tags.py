04b8e23616883297b20323d5e85160d967a2b1e8
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2023-09-25 16:14:52 +0100

    Make build tag processing output point to obj dir

--- build_tools/process_build_tags.py
+++ build_tools/process_build_tags.py
@@ -1,22 +1,26 @@
 import re
 import sys
+import os
 
-def parse_file(filename, data, categories):
+def parse_file(filename, data, categories, obj_dir):
   with open(filename, "r") as f:
     for line in f:
       match = re.search("@build (.+)", line)
       if match:
         m = match.group(1).strip().upper()
         if m in categories:
-          data[m].append(filename.replace(".f90", ".o"))
+          filename = filename.replace(".f90", ".o")
+          filename = obj_dir + "/" + os.path.basename(filename)
+          data[m].append(filename)
 
 if __name__ == "__main__":
   suffix = "_OBJ"
   obj_categories = ["CAF", "MPI", "MPI+OMP", "PETSC"]
+  obj_dir = sys.argv[1]
 
   data = {x:[] for x in obj_categories}
-  for filename in sys.argv[1:]:
-    parse_file(filename, data, obj_categories)
+  for filename in sys.argv[2:]:
+    parse_file(filename, data, obj_categories, obj_dir)
 
   for k,v in data.items():
     print(k + suffix, "=", " ".join(set(v)))
