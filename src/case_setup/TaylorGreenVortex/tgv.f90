aaa58fae7e5417aab63c76f0aa8380503b710668
Shrutee Lakshminarasimha <shrutee.lakshminarasimha2@rolls-royce.com>
2023-10-11 07:51:08 +0100

    TGV 3D: working fine

--- src/case_setup/TaylorGreenVortex/tgv.f90
+++ src/case_setup/TaylorGreenVortex/tgv.f90
@@ -13,7 +13,7 @@ program tgv
                          compute_bwidth
   use constants, only: cell, face, ccsconfig, ccs_string_len, geoext, adiosconfig, ndim, &
                        field_u, field_v, field_w, field_p, field_p_prime, field_mf, field_viscosity, &
-                       cell_centred_central, cell_centred_upwind, face_centred
+                       field_density, cell_centred_central, cell_centred_upwind, face_centred
   use constants, only: ccs_split_type_shared, ccs_split_type_low_high, ccs_split_undefined
   use fields, only: create_field, set_field_config_file, set_field_n_boundaries, set_field_name, &
                     set_field_type, set_field_vector_properties, set_field_store_residuals, set_field_enable_cell_corrections
@@ -58,7 +58,7 @@ program tgv
   type(vector_spec) :: vec_properties
 
   type(field_spec) :: field_properties
-  class(field), allocatable, target :: u, v, w, p, p_prime, mf, viscosity
+  class(field), allocatable, target :: u, v, w, p, p_prime, mf, viscosity, density
 
   type(field_ptr), allocatable :: output_list(:)
 
@@ -187,6 +187,8 @@ program tgv
   call create_field(field_properties, p_prime)
   call set_field_name("viscosity", field_properties)
   call create_field(field_properties, viscosity)
+  call set_field_name("density", field_properties)
+  call create_field(field_properties, density)
 
   call set_vector_location(face, vec_properties)
   call set_size(par_env, mesh, vec_properties)
@@ -206,7 +208,7 @@ program tgv
 
   ! Initialise velocity field
   if (irank == par_env%root) print *, "Initialise velocity field"
-  call initialise_flow(mesh, u, v, w, p, mf, viscosity)
+  call initialise_flow(mesh, u, v, w, p, mf, viscosity, density)
   call calc_kinetic_energy(par_env, mesh, u, v, w)
   call calc_enstrophy(par_env, mesh, u, v, w)
 
@@ -230,7 +232,7 @@ program tgv
   call set_fluid_solver_selector(field_v, v_sol, fluid_sol)
   call set_fluid_solver_selector(field_w, w_sol, fluid_sol)
   call set_fluid_solver_selector(field_p, p_sol, fluid_sol)
-  call allocate_fluid_fields(7, flow_fields)
+  call allocate_fluid_fields(8, flow_fields)
   call set_field(1, field_u, u, flow_fields)
   call set_field(2, field_v, v, flow_fields)
   call set_field(3, field_w, w, flow_fields)
@@ -238,6 +240,7 @@ program tgv
   call set_field(5, field_p_prime, p_prime, flow_fields)
   call set_field(6, field_mf, mf, flow_fields)
   call set_field(7, field_viscosity, viscosity, flow_fields)
+  call set_field(8, field_density, density, flow_fields)
 
   call timer_stop(timer_index_init)
   call timer_register("I/O time for solution", timer_index_io_sol)
@@ -393,7 +396,7 @@ contains
 
   end subroutine
 
-  subroutine initialise_flow(mesh, u, v, w, p, mf, viscosity)
+  subroutine initialise_flow(mesh, u, v, w, p, mf, viscosity, density)
 
     use constants, only: insert_mode, ndim
     use types, only: vector_values, cell_locator, face_locator, neighbour_locator
@@ -406,7 +409,7 @@ contains
 
     ! Arguments
     class(ccs_mesh), intent(in) :: mesh
-    class(field), intent(inout) :: u, v, w, p, mf, viscosity
+    class(field), intent(inout) :: u, v, w, p, mf, viscosity, density
 
     ! Local variables
     integer(ccs_int) :: n, count
@@ -417,7 +420,7 @@ contains
     type(face_locator) :: loc_f
     type(neighbour_locator) :: loc_nb
     type(vector_values) :: u_vals, v_vals, w_vals, p_vals
-    real(ccs_real), dimension(:), pointer :: mf_data, viscosity_data
+    real(ccs_real), dimension(:), pointer :: mf_data, viscosity_data, density_data
 
     real(ccs_real), dimension(ndim) :: x_p, x_f
     real(ccs_real), dimension(ndim) :: face_normal
@@ -511,12 +514,17 @@ contains
     viscosity_data(:) =  1.e-2_ccs_real
     call restore_vector_data(viscosity%values, viscosity_data)
 
+    call get_vector_data(density%values, density_data)
+    density_data(:) = 1.0_ccs_real
+    call restore_vector_data(density%values, density_data)
+
     call update(u%values)
     call update(v%values)
     call update(w%values)
     call update(p%values)
     call update(mf%values)
     call update(viscosity%values)
+    call update(density%values)
 
   end subroutine initialise_flow
 
