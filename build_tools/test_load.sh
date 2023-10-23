8cb4be5b0d04b9c5a3825e2bcd4ee8f753767170
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2023-10-23 14:40:22 +0100

    add comment and output top 10 proc in test_load.sh

--- build_tools/test_load.sh
+++ build_tools/test_load.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exit $(echo $(awk '{print $1}' /proc/loadavg) '>= 1.0' | bc -l)
+# test if average load over last minute is >= 1, i.e. if more than 1 core is doing something
+if [ $(echo $(awk '{print $1}' /proc/loadavg) '>= 1.0' | bc -l) ] then
+    echo "Load detected on the system:"
+    ps -eo pid,user,%cpu,cmd --sort -%cpu | head -n 10
+    exit 1
+fi
