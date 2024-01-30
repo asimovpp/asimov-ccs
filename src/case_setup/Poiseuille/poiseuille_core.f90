38be944826e174eea1bef6a1cc3e26ac5385e187
Shrutee Lakshminarasimha <shrutee.lakshminarasimha2@rolls-royce.com>
2024-01-30 06:11:21 +0000

    Poiseuille: removing unwanted statements

--- src/case_setup/Poiseuille/poiseuille_core.f90
+++ src/case_setup/Poiseuille/poiseuille_core.f90
@@ -166,18 +166,7 @@ module poiseuille_core
     call set_field_enable_cell_corrections(enable_cell_corrections, field_properties)
 
     call set_field_vector_properties(vec_properties, field_properties)
-    !call set_field_name("u", field_properties)
-    !call create_field(field_properties, u)
-    !call set_field_name("v", field_properties)
-    !call create_field(field_properties, v)
-    !call set_field_name("w", field_properties)
-    !call create_field(field_properties, w)
-
     call set_field_type(cell_centred_central, field_properties)
-    !call set_field_name("p", field_properties)
-    !call create_field(field_properties, p)
-    !call set_field_name("p_prime", field_properties)
-    !call create_field(field_properties, p_prime)
     call set_field_name("viscosity", field_properties)
     call create_field(field_properties, viscosity) 
     call set_field_name("density", field_properties)
@@ -201,7 +190,6 @@ module poiseuille_core
     do i = 1, size(field_list)
       if(field_list(i)%name == 'u') then
         u%ptr => field_list(i)%f
-        !print*,field_list(i)%name
       end if 
     end do
 
@@ -220,24 +208,14 @@ module poiseuille_core
     call set_field_name("mf", field_properties)
     call create_field(field_properties, mf)
 
-    ! Add fields to output list
-    !call add_field_to_outputlist(u, "u", output_list)
-    !call add_field_to_outputlist(v, "v", output_list)
-    !call add_field_to_outputlist(w, "w", output_list)
-    !call add_field_to_outputlist(p, "p", output_list)
-
     do i = 1, size(field_list)
       call add_field_to_outputlist(field_list(i)%f, field_list(i)%name, output_list)
     end do
 
     ! Initialise velocity field
     if (irank == par_env%root) print *, "Initialise velocity field"
-    !call initialise_flow(u, v, w, p, mf, viscosity, density)
     call initialise_flow(field_list, mf, viscosity, density)
-    !call update(u%values)
-    !call update(v%values)
-    !call update(w%values)
-    !call update(p%values)
+
     do i = 1, size(field_list)
       call update(field_list(i)%f%values)
     end do
@@ -261,13 +239,6 @@ module poiseuille_core
       end if 
     end do
 
-    !call get_local_num_cells(local_num_cells)
-    !call get_vector_data(u%values, u_data)
-    !do index_p = 1, local_num_cells
-      !print*,"u=",u_data(index_p)
-    !end do
-    !call restore_vector_data (u%values, u_data)
-
     call calc_kinetic_energy(par_env, u%ptr, v%ptr, w%ptr)
     call calc_enstrophy(par_env, u%ptr, v%ptr, w%ptr)
 
@@ -290,11 +261,6 @@ module poiseuille_core
     call set_fluid_solver_selector(field_w, w_sol, fluid_sol)
     call set_fluid_solver_selector(field_p, p_sol, fluid_sol)
 
-    !call add_field(u, flow_fields)
-    !call add_field(v, flow_fields)
-    !call add_field(w, flow_fields)
-    !call add_field(p, flow_fields)
-    !call add_field(p_prime, flow_fields)
     do i = 1, size(field_list)
       call add_field(field_list(i)%f, flow_fields)
     end do
@@ -316,11 +282,6 @@ module poiseuille_core
     call timer_stop(timer_index_sol)
 
     ! Clean-up
-    !deallocate (u)
-    !deallocate (v)
-    !deallocate (w)
-    !deallocate (p)
-    !deallocate (p_prime)
     do i = 1, size(field_list)
       deallocate(field_list(i)%f)
     end do
