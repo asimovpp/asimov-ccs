ff014622be6df228f97381ea10d18d64efb1677b
Shrutee Lakshminarasimha <shrutee.lakshminarasimha2@rolls-royce.com>
2024-02-01 09:27:38 +0000

    resolving errors 3

--- src/case_setup/TaylorGreenVortex/tgv.f90
+++ src/case_setup/TaylorGreenVortex/tgv.f90
@@ -60,7 +60,7 @@ program tgv
   type(vector_spec) :: vec_properties
 
   type(field_spec) :: field_properties
-  class(field), allocatable, target :: u, v, w, p, p_prime, mf, viscosity, density
+  class(field), allocatable, target :: u, v, w, mf, viscosity, density
 
   type(field_ptr), allocatable :: output_list(:)
   type(field_elt), allocatable, target :: field_list(:)
