8ee7b11f11863f2f365bf3c542ab21dbe1825582
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2024-03-07 16:01:37 +0000

    Add no-reordering reordering submodule

---
+++ src/meshing/reordering/none/reordering_none.f90
@@ -0,0 +1,25 @@
+submodule(reordering) reordering_none
+
+  implicit none
+
+contains
+
+  !v Creates a no-action reordering list
+  module subroutine get_reordering(mesh, new_indices)
+
+    use meshing, only: get_local_num_cells
+
+    type(ccs_mesh), intent(in) :: mesh                                      !< the mesh to be reordered
+    integer(ccs_int), dimension(:), allocatable, intent(out) :: new_indices !< new indices in "to(from)" format
+
+    integer(ccs_int) :: local_num_cells
+    integer(ccs_int) :: i
+
+    call get_local_num_cells(mesh, local_num_cells)
+
+    allocate (new_indices(local_num_cells))
+    new_indices = (/(i, i=1,local_num_cells)/)
+
+  end subroutine get_reordering
+
+end submodule
