ad6dc9b1fa8e2ebbf6b0358fb749fbd5d7242534
Alexei <a.borissov@epcc.ed.ac.uk>
2024-04-18 11:26:14 +0100

    actually add the new mesh initialisation file this time

--- src/ccs_core/core_mod.f90
+++ src/ccs_core/core_mod.f90
@@ -7,5 +7,15 @@ module core
   implicit none
 
   private
-  
+  public: initialise_mesh
+
+  interface
+
+    module subroutine initialise_mesh(par_env, shared_env, run_options)
+      type(parallel_environment), intent(in) :: par_env
+      type(parallel_environment), intent(in) :: shared_env
+      type(ccs_options), intent(in) :: run_options
+    end subroutine initialise_mesh
+
+  end interface
 end module core
