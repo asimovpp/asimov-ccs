76bf2ed1628116a976dff91f0cf1a6949598f820
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2024-04-18 17:31:25 +0100

    put initial value getter functions as arguments

--- src/ccs_core/core_mod.f90
+++ src/ccs_core/core_mod.f90
@@ -4,9 +4,37 @@
 
 module core
 
-  use flow_init
   implicit none
 
   private
+  public :: core_initialise_flow
+  public :: core_initialise_mass_flux
+
+  interface
+
+    !> Initialise every cell based field prompting values from get_init_flow
+    module subroutine core_initialise_flow(flow_fields, get_init_flow)
+      type(fluid), intent(inout) :: flow_fields
+      interface 
+        pure subroutine get_init_flow(x_p, field_name, init_val)
+          real(ccs_real), dimension(ndim), intent(in) :: x_p
+          character(len=*), intent(in) :: field_name
+          real(ccs_real), intent(inout) :: init_val
+        end subroutine
+      end interface
+    end subroutine
+
+    !> Initialise mass flux field using calling get_init_mass_flux
+    module subroutine core_initialise_mass_flux(flow_fields, get_init_mass_flux)
+      type(fluid), intent(inout) :: flow_fields
+      interface
+        pure subroutine get_init_mass_flux(index_f, init_flux)
+          integer(ccs_int), intent(in) :: index_f
+          real(ccs_real), intent(inout) :: init_flux
+        end subroutine
+      end interface
+    end subroutine
+
+  end interface
   
 end module core
--- src/ccs_core/flow_init_mod.f90
+++ src/ccs_core/flow_init_mod.f90
@@ -1,9 +1,11 @@
 module flow_init
+submodule(core) initialise_flow
 
   use kinds, only: ccs_int
   use case_setup, only: get_init_flow, get_init_mass_flux
 
 
+  use fields, only: get_field_is_face_based
   use kinds, only: ccs_real, ccs_int
   use types, only: fluid, field, cell_locator, face_locator, neighbour_locator, vector_values
 
@@ -25,10 +27,18 @@ module flow_init
 
   contains
 
-  subroutine initialise_flow(flow_fields)
+  module subroutine core_initialise_flow(flow_fields, get_init_flow)
 
     ! Arguments
     type(fluid), intent(inout) :: flow_fields
+
+    interface 
+      pure subroutine get_init_flow(x_p, field_name, init_val)
+        real(ccs_real), dimension(ndim), intent(in) :: x_p
+        character(len=*), intent(in) :: field_name
+        real(ccs_real), intent(inout) :: init_val
+      end subroutine
+    end interface
 
     ! Local variables
     class(field), pointer :: current_field
@@ -38,12 +48,19 @@ module flow_init
     type(cell_locator) :: loc_p
     real(ccs_real), dimension(ndim) :: x_p
     type(vector_values):: values
+    logical :: is_face_field
 
     call get_local_num_cells(n_local)
 
     do i_field=1, size(flow_fields%fields)
 
       call get_field(flow_fields, i_field, current_field)
+
+      call get_field_is_face_based(current_field, is_face_field)
+      ! Skip face centered fields
+      if (is_face_field) then
+        continue
+      end if
 
       call create_vector_values(n_local, values)
       call set_mode(insert_mode, values)
@@ -72,10 +89,18 @@ module flow_init
 
   end subroutine initialise_flow
 
-  subroutine initialise_mass_flux(flow_fields)
+  module subroutine core_initialise_mass_flux(flow_fields, get_init_mass_flux)
 
     type(fluid), intent(inout) :: flow_fields
-    real(ccs_real) :: area, density
+
+    interface
+      pure subroutine get_init_mass_flux(index_f, init_flux)
+        integer(ccs_int), intent(in) :: index_f
+        real(ccs_real), intent(inout) :: init_flux
+      end subroutine
+    end interface
+
+    real(ccs_real) :: area
     real(ccs_real), dimension(ndim) :: velocity
 
     class(field), pointer :: u, v, w
@@ -96,7 +121,6 @@ module flow_init
     call get_field(flow_fields, "v", v)
     call get_field(flow_fields, "w", w)
     call get_field(flow_fields, "mf", mf)
-    call get_field(flow_fields, "density", rho)
     call get_vector_data(mf%values, mf_data)
 
     ! Loop over local cells and faces
@@ -111,31 +135,33 @@ module flow_init
         call get_local_index(loc_nb, index_nb)
 
         call get_boundary_status(loc_nb, is_boundary)
+        call create_face_locator(index_p, j, loc_f)
+        call get_local_index(loc_f, index_f)
+        call get_face_normal(loc_f, face_normal)
+        call get_face_area(loc_f, area)
 
-        if (index_p > index_nb) then  
+        if (is_boundary) then
+          ! Get boundary values
+          call compute_boundary_values(current_field, 1, loc_p, loc_f, face_normal, velocity(1))
+          call compute_boundary_values(current_field, 2, loc_p, loc_f, face_normal, velocity(2))
+          call compute_boundary_values(current_field, 3, loc_p, loc_f, face_normal, velocity(3))
 
-          call create_face_locator(index_p, j, loc_f)
-          call get_local_index(loc_f, index_f)
-          call get_face_normal(loc_f, face_normal)
+          mf_data(index_f) = area * dot_product(velocity, face_normal)
+          call get_init_mass_flux(index_f, mf_data(index_f))
+
+        else if (index_p < index_nb) then  
           call get_centre(loc_f, x_f)
           call get_face_interpolation(loc_f, interpol_factor)
 
           u_p = [u%values_ro(index_p), v%values_ro(index_p), w%values_ro(index_p)]
           u_nb = [u%values_ro(index_nb), v%values_ro(index_nb), w%values_ro(index_nb)]
           velocity = interpol_factor * u_p + (1-interpol_factor) * u_nb
-          density = interpol_factor * rho%values_ro(index_p) + (1-interpol_factor) * rho%values_ro(index_nb)
-          call get_face_area(loc_f, area)
 
           ! compute initial value based on current face coordinates
-          mf_data(index_f) = density * area * dot_product(velocity, face_normal)
+          mf_data(index_f) = area * dot_product(velocity, face_normal)
 
           ! Allow case to overwrite interpolated value
           call get_init_mass_flux(index_f, mf_data(index_f))
-
-        else if (is_boundary) then
-          continue
-
-
         end if
 
       end do
@@ -148,4 +174,4 @@ module flow_init
 
 
 
-end module
+end submodule
