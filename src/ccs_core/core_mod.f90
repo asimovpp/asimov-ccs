5ed6c8071b255a471e1822694377c06c533b0617
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2024-04-19 09:05:11 +0100

    Add a generic parallel configuration subroutine

--- src/ccs_core/core_mod.f90
+++ src/ccs_core/core_mod.f90
@@ -12,12 +12,20 @@ module core
 
   private
 
-  public :: run_solver
   public :: ccs_options
+  public :: configure_parallelism
+  public :: run_solver
 
   !v Type to contain the configuration of the CCS run
   type :: ccs_options
+    !! Generic/misc
     character(len=:), allocatable :: case_path
+
+    !! Parallel-related
+    logical :: use_mpi_splitting
+    integer :: split_type
+
+    !! Solver-related
     integer(ccs_int) :: num_steps
     real(ccs_real) :: dt
     integer(ccs_int) :: write_frequency
@@ -27,6 +35,16 @@ module core
   end type ccs_options
 
   interface
+    !v Subroutine to configure sub parallel environments.
+    !
+    ! Currently this only configures the shared environment, but it could be extended for particle
+    ! environments, etc.
+    module subroutine configure_parallelism(run_options, par_env, shared_env)
+      type(ccs_options), intent(in) :: run_options
+      class(parallel_environment), allocatable, intent(in) ::  par_env
+      class(parallel_environment), allocatable, intent(out) :: shared_env
+    end subroutine configure_parallelism
+    
     !v Subroutine to run a flow problem.
     !
     ! This is responsible for the time loop, calling post-processing subroutines and performing
