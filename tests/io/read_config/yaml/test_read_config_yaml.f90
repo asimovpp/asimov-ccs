3802eab3cfeec704d30659cdc1fc7d580a512af1
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2024-07-13 15:59:18 +0100

    Add unit test for reading yaml values

---
+++ tests/io/read_config/yaml/test_read_config_yaml.f90
@@ -0,0 +1,89 @@
+!v Test program for the yaml parser.
+
+program test_read_config_yaml
+
+  use testing_lib
+
+  use fortran_yaml_c_interface, only: parse
+  use read_config, only: get_value
+  
+  implicit none
+
+  character(len=*), parameter :: config_yaml = "config.yml"
+  class(*), pointer :: config_file !< Pointer to config file
+  
+  call init()
+  call setup()
+
+  call test_read_values(config_file)
+  
+  call fin()
+
+contains
+
+  ! Perform test-case setup
+  subroutine setup()
+
+    character(len=:), allocatable :: error
+
+    ! Open config file
+    config_file => parse(config_yaml, error)
+    if (allocated(error)) then
+      call stop_test(trim(error))
+    end if
+
+  end subroutine setup
+
+  subroutine test_read_values(conf_file)
+
+    class(*), pointer, intent(in) :: conf_file
+
+    integer :: ival
+    real(ccs_real) :: rval
+    character(len=:), allocatable :: sval
+    logical :: lval
+
+    logical :: val_present
+
+    call get_value(conf_file, "integer", ival, val_present, required=.true.)
+    if (.not. val_present) then
+      call stop_test("Integer value was not found")
+    end if
+    if (ival /= 42) then
+      call stop_test("Integer value does not match expectation")
+    end if
+
+    call get_value(conf_file, "real", rval, val_present, required=.true.)
+    if (.not. val_present) then
+      call stop_test("Real value was not found")
+    end if
+    if (rval /= 3.14_ccs_real) then
+      call stop_test("Real value does not match expectation")
+    end if
+
+    call get_value(conf_file, "string", sval, val_present, required=.true.)
+    if (.not. val_present) then
+      call stop_test("String value was not found")
+    end if
+    if (sval /= "Hello world!") then
+      call stop_test("String value does not match expectation")
+    end if
+    
+    call get_value(conf_file, "yes", lval, val_present, required=.true.)
+    if (.not. val_present) then
+      call stop_test("Logical (true) value was not found")
+    end if
+    if (.not. lval) then
+      call stop_test("Logial (true) value does not match expectation")
+    end if
+    call get_value(conf_file, "no", lval, val_present, required=.true.)
+    if (.not. val_present) then
+      call stop_test("Logical (false) value was not found")
+    end if
+    if (lval) then
+      call stop_test("Logial (false) value does not match expectation")
+    end if
+    
+  end subroutine test_read_values
+  
+end program test_read_config_yaml
