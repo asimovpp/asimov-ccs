e9f531abad94aa7fd9952a0bc30d418dd3c391a3
Michele Weiland <mweiland@Micheles-MacBook-Air.local>
2024-07-19 18:09:53 +0100

    Adding 'use core' once more

--- tests/nightly/verification/test_poiseuille_disturb_cartesian.f90
+++ tests/nightly/verification/test_poiseuille_disturb_cartesian.f90
@@ -4,6 +4,7 @@
 program test_poiseuille_disturb_cartesian
 #include "ccs_macros.inc"
 
+  use core
   use testing_lib
   use error_analysis, only: get_order, print_error_summary, disturb_cartesian
   use mesh_utils, only: build_square_mesh
