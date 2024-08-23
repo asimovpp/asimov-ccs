9e7b70af4fa566e41706e84d4343d324e5dd58e0
Erich Essmann <essmanne@gmail.com>
2024-08-23 11:24:46 +0100

    Interfaces fixed

--- src/fv_schemes/fv_kernels.f90
+++ src/fv_schemes/fv_kernels.f90
@@ -1,10 +1,23 @@
 module fv_kernels
-    use types
-    implicit none
+  use types
+  implicit none
   !> Advection kernel
-    type, extends(abstract_kernel) :: advection_kernel
-    contains
-        procedure :: coeffs => advection_coeffs
-        procedure :: eval => advection_eval
-    end type advection_kernel
+  type, extends(abstract_kernel) :: advection_kernel
+  contains
+     procedure :: coeffs => advection_coeffs
+     procedure :: eval => advection_eval
+  end type advection_kernel
+
+  interface
+     module pure function advection_coeffs(this) result(coeffs)
+        class(advection_kernel), intent(in) :: this
+        real, allocatable :: coeffs(:)
+     end function advection_coeffs
+
+     module subroutine advection_eval(this, result)
+        class(advection_kernel), intent(in) :: this
+        real, intent(out) :: result
+     end subroutine advection_eval
+  end interface
+
 end module fv_kernels
