61245ad2a3ba917e689db2a79e1975824748c607
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2024-11-06 22:02:14 +0000

    Add documentation comments to core_parallel

--- src/ccs_core/core_parallel.f90
+++ src/ccs_core/core_parallel.f90
@@ -8,9 +8,13 @@ submodule(core) core_parallel
 
 contains
   
+  !v Subroutine to configure sub parallel environments.
+  !
+  ! Currently this only configures the shared environment, but it could be extended for particle
+  ! environments, etc.
   module subroutine configure_parallelism(run_options, par_env, shared_env)
 
-    type(ccs_options), intent(in) :: run_options
+    type(ccs_options), intent(in) :: run_options                        !< The runtime configuration
     class(parallel_environment), allocatable, intent(in) ::  par_env    !< The main parallel environment
     class(parallel_environment), allocatable, intent(out) :: shared_env !< The shared parallel environment
 
@@ -18,9 +22,10 @@ contains
 
   end subroutine configure_parallelism
 
+  !> Internal implementation to configure parallelism based on the parallelism options set at runtime.
   subroutine configure_parallelism_impl(par_opt, par_env, shared_env)
 
-    type(parallel_options), intent(in) :: par_opt
+    type(parallel_options), intent(in) :: par_opt                       !< The runtime parallelism options
     class(parallel_environment), allocatable, intent(in) ::  par_env    !< The main parallel environment
     class(parallel_environment), allocatable, intent(out) :: shared_env !< The shared parallel environment
 
