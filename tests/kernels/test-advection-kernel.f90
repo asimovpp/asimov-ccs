b82fe40ad50c78a9c1a6d4b7634e82a312529251
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2024-11-12 13:32:32 +0000

    rename to x_P etc.

--- tests/kernels/test-advection-kernel.f90
+++ tests/kernels/test-advection-kernel.f90
@@ -9,7 +9,7 @@ program test_advection_kernel
     real(ccs_real) :: phi_P, phi_P_prime, interpol_factor
     real(ccs_real) :: phi_N
     real(ccs_real) :: dx
-    real(ccs_real), dimension(3) :: N_loc, P_loc, f_loc
+    real(ccs_real), dimension(3) :: x_N, x_P, x_f
     real(ccs_real), dimension(2) :: coeffs
     real(ccs_real) :: rhs
     real(ccs_real), dimension(num_iters) :: errors
@@ -26,15 +26,15 @@ program test_advection_kernel
 
     do i = 1, num_iters
         dx = 1.0_ccs_real / real(i)**2
-        N_loc = P_loc + [dx, 0, 0]
-        f_loc = P_loc + [dx/3, 0, 0]
+        x_N = x_P + [dx, 0, 0]
+        x_f = x_P + [dx/3, 0, 0]
 
         phi_N = phi(dx)
 
-        call advection%eval([phi_P, phi_N], [u_P, u_N], P_loc, N_loc, f_loc, &
+        call advection%eval([phi_P, phi_N], [u_P, u_N], x_P, x_N, x_f, &
                             interpol_factor, is_boundary=.false., coeffs, rhs)
 
-        call get_error(coeffs, rhs, f_loc, errors(i))
+        call get_error(coeffs, rhs, x_f, errors(i))
         refinements(i) = dx
     end do
 
@@ -65,14 +65,14 @@ contains
         grad_phi = 2*cos(2*x + 17)
     end function grad_phi
 
-    subroutine get_error(coeffs, rhs, f_loc, error)
+    subroutine get_error(coeffs, rhs, x_f, error)
         real(ccs_real), intent(in) :: coeffs(2), rhs
-        real(ccs_real), intent(in) :: f_loc(3)
+        real(ccs_real), intent(in) :: x_f(3)
         real(ccs_real), intent(out) :: error
         real(ccs_real) :: analytical, error
 
-        analytical = u(f_loc(1)) * grad_phi(f_loc(1))
-        error = abs(analytical - (rhs - coeffs(2) * phi(f_loc(1))) / coeffs(1))
+        analytical = u(x_f(1)) * grad_phi(x_f(1))
+        error = abs(analytical - (rhs - coeffs(2) * phi(x_f(1))) / coeffs(1))
     end subroutine get_error
 
 end program test_advection_kernel
