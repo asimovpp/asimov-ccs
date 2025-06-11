d42541e94ee1cf8088d4d3605371f7f210aa0322
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2025-06-11 14:03:25 +0100

    fix scalar transport imports

--- src/case_setup/ScalarTransport/scalar_transport.f90
+++ src/case_setup/ScalarTransport/scalar_transport.f90
@@ -21,7 +21,7 @@ program scalar_transport
                      get_local_index, get_boundary_status, get_face_normal, nullify_mesh_object, &
                      get_local_num_cells
   use scalars, only: update_scalars
-  use utils, only: exit_print, add_field_to_outputlist, &
+  use utils, only: exit_print, add_field_to_outputlist
   use fields, only: get_field, dealloc_fluid_fields
   use timers, only: timer_init, timer_register_start, timer_stop, &
                     timer_print_all, timer_export_csv
