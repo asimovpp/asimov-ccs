69783bd82a841d90dc97273861fac6395694e5aa
svande <s.vandebund@epcc.ed.ac.uk>
2026-03-30 14:16:28 +0100

    Add initial logging utility to store file unit

---
+++ src/tools/logging.f90
@@ -0,0 +1,59 @@
+!v Logging utility to specify where to redirect output.
+module logging
+#include "ccs_macros.inc"
+
+  use iso_fortran_env, only : output_unit, error_unit
+
+  implicit none
+
+  logical :: initialised = .false.
+
+  integer :: log_unit_out = output_unit
+  integer :: log_unit_err = error_unit
+
+  contains
+
+  !> Initialise logging module
+  subroutine initialise_logging(par_env, file_name)
+    use parallel_types, only: parallel_environment
+    use parallel, only: is_root
+    use utils, only: exit_print
+
+    implicit none
+
+    class(parallel_environment), allocatable, intent(in) :: par_env !< parallel environment
+    character(len=*), intent(in) :: file_name
+    integer :: ierr
+
+    if (is_root(par_env)) then
+      open(newunit=log_unit_out, file=file_name, status="replace", iostat=ierr)
+      if (ierr /= 0) then
+        call error_abort("ERROR: Could not open log file " // file_name)
+      end if
+      initialised = .true.
+    end if
+
+  end subroutine initialise_logging
+
+  !> Finalise logging module
+  subroutine finalise_logging(par_env)
+    use parallel_types, only: parallel_environment
+    use parallel, only: is_root
+    use utils, only: exit_print
+
+    implicit none
+
+    class(parallel_environment), allocatable, intent(in) :: par_env !< parallel environment
+    integer :: ierr
+
+    if (is_root(par_env)) then
+      close(log_unit_out, iostat=ierr)
+      if (ierr /= 0) then
+        call error_abort("ERROR: Could not close log file.")
+      end if
+      initialised = .false.
+    end if
+
+  end subroutine finalise_logging
+
+end module logging
