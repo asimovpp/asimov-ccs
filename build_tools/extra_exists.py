7d6e34e211151fcf43b5f0a4dd22d388eaa182fe
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2026-03-30 16:14:04 +0100

    Added docstring to new python script

--- build_tools/extra_exists.py
+++ build_tools/extra_exists.py
@@ -1,12 +1,22 @@
+"""
+This script takes two arguments:
+    - inputfile: the yaml config file with CCS configuration
+    - extra_to_test: a valid config.yaml "options" value
+                     (see build_tools/config_mapping.yaml)
+This script will then see if the `extra_to_test` value exists in the
+`inputfile` passed and return:
+    int: 0 - if `extra_to_test` is not found in `inputfile`
+    int: 1 - if `extra_to_test` is found in `inputfile`
+"""
+
 import sys
 import yaml
-
 
 
 if __name__=="__main__":
   inputfile = sys.argv[1]
   extra_to_test = sys.argv[2]
-
+        
   assert isinstance(extra_to_test, str)
 
   with open(inputfile, "r") as f:
@@ -15,6 +25,7 @@ if __name__=="__main__":
   result = 0
 
   if "options" in config:
+    # The first check to avoid ValueErrors
     if extra_to_test in config["options"].values():
       result = 1
 
