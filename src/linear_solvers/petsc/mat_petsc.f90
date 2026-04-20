c388260a7aa5567c6e55c1e7eba15309a996bc24
Paul <p.bartholomew@epcc.ed.ac.uk>
2026-04-20 16:28:58 +0100

    Try a neater solution

--- src/linear_solvers/petsc/mat_petsc.f90
+++ src/linear_solvers/petsc/mat_petsc.f90
@@ -12,8 +12,6 @@ submodule(mat) mat_petsc
   use error_codes
   use logging, only: log_unit_out
 
-  use iso_c_binding
-
   implicit none
 
 contains
@@ -599,11 +597,8 @@ contains
   end subroutine mat_vec_product
 
   module subroutine check_operator_symmetry(M)
-#if PETSC_VERSION_GE(3,23,0)
-#include <petsc/finclude/petscsysbase.h>
-#else
 #include <petsc/finclude/petscsys.h>
-#endif
+    use petscsys
     use petscmat, only: MatIsSymmetric
 
     class(ccs_matrix), intent(in) :: M
