458a7159fc2b8a3db570af615d210d2e3a370775
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2026-05-11 16:51:42 +0100

    add some bc_types

--- src/ccs_core/bc_constants_mod.f90
+++ src/ccs_core/bc_constants_mod.f90
@@ -8,12 +8,15 @@ module bc_constants
 
   implicit none
 
-  integer(ccs_int), parameter :: bc_type_periodic = 1
-  integer(ccs_int), parameter :: bc_type_sym = 2
-  integer(ccs_int), parameter :: bc_type_dirichlet = 3
-  integer(ccs_int), parameter :: bc_type_neumann = 4
-  integer(ccs_int), parameter :: bc_type_extrapolate = 5
+  integer(ccs_int), parameter :: bc_type_dirichlet = 1
+  integer(ccs_int), parameter :: bc_type_neumann = 2
+  integer(ccs_int), parameter :: bc_type_extrapolate = 3
+  integer(ccs_int), parameter :: bc_type_sym = 4
+  integer(ccs_int), parameter :: bc_type_inflow = 5
+  integer(ccs_int), parameter :: bc_type_outflow = 6
   integer(ccs_int), parameter :: bc_type_wall = 7
-  integer(ccs_int), parameter :: bc_type_profile = 8
+  integer(ccs_int), parameter :: bc_type_slip_wall = 8
+  integer(ccs_int), parameter :: bc_type_periodic = 9
+  integer(ccs_int), parameter :: bc_type_profile = 10
 
 end module bc_constants
